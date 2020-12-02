4966e8ba8ff6902cf1c7ce788de7e6a92c92de75
Eric Montague <eric.g.montague@gmail.com>
2020-12-02 00:50:00 -0500

    Added classes to shell context

--- api/chat_app.py
+++ api/chat_app.py
@@ -3,11 +3,56 @@
 
 import os
 from app import create_app
+from app.models import (
+    ChatRequest,
+    ChatRequestStatus,
+    PrivateChat,
+    GroupChat,
+    Community,
+    Topic,
+    Image,
+    ImageType,
+    Message,
+    Reaction,
+    Notification,
+    NotificationType,
+    Role,
+    RolePermission,
+    RoleName,
+    User,
+    Location,
+)
+from app.clients import dynamodb_client
 
 
 app = create_app(os.environ.get("FLASK_CONFIG", "development"))
 
 
+@app.shell_context_processor
+def make_shell_context():
+    """Allow models and other objects to be globally
+    accessible in the Flask shell.
+    """
+    return dict(
+        dynamodb_client=dynamodb_client,
+        ChatRequest=ChatRequest,
+        ChatRequestStatus=ChatRequestStatus,
+        PrivateChat=PrivateChat,
+        GroupChat=GroupChat,
+        Community=Community,
+        Topic=Topic,
+        Image=Image,
+        ImageType=ImageType,
+        Message=Message,
+        Reaction=Reaction,
+        Notification=Notification,
+        NotificationType=NotificationType,
+        Role=Role,
+        RolePermission=RolePermission,
+        RoleName=RoleName,
+        User=User,
+        Location=Location,
+    )
 
 
 if __name__ == "__main__":
