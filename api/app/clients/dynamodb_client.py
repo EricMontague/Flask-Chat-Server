4f0ba5a113b8e09860bf9f7a33db5d7ff1056082
Eric Montague <eric.g.montague@gmail.com>
2020-12-02 00:51:23 -0500

    Started working on creating a dynamodb wrapper class

---
+++ api/app/clients/dynamodb_client.py
@@ -0,0 +1,57 @@
+"""This module contains a class that acts as a wrapper around
+the boto3 library's DynamoDB resource.
+"""
+
+import boto3
+
+
+class _DynamoDBClient:
+    """Class that acts as a wrapper around the boto3 library's
+    DynamoDB resource.
+    """
+
+    _USERS_TABLE = "Users"
+
+    def __init__(self, endpoint_url=""):
+        # Endpoint url used for testing with a local version of DynamoDB
+        if endpoint_url:
+            self._dynamodb = boto3.resource("dynamodb", endpoint_url=endpoint_url)
+        else:
+            self._dynamodb = boto3.resource("dynamodb")
+        self._users_table = self._dynamodb.Table(self._USERS_TABLE)
+
+    def get_user(self, partition_key):
+        response = self._users_table.get_item(
+            Key={"id": partition_key}, ReturnConsumedCapacity="INDEXES"
+        )
+        return response, response["ResponseMetadata"]["HTTPStatusCode"]
+
+    def put_user(self, user):
+        response = self._users_table.put_item(
+            Item=user.to_dict(),
+            ReturnConsumedCapacity="INDEXES",
+            ReturnItemCollectionMetrics="SIZE",
+        )
+        return response, response["ResponseMetadata"]["HTTPStatusCode"]
+
+    def update_user(self, partition_key, expression):
+        response = self._users_table.update_item(
+            Key={"id": partition_key},
+            ReturnConsumedCapacity="INDEXES",
+            ReturnItemCollectionMetrics="SIZE",
+            UpdateExpression=expression.update_expression,
+            ExpressionAttributeNames=expression.attribute_names,
+            ExpressionAttributeValues=expression.attribute_values
+        )
+        return response, response["ResponseMetadata"]["HTTPStatusCode"]
+
+    def delete_user(self, partition_key):
+        response = self._users_table.delete_item(
+            Key={"id": partition_key},
+            ReturnConsumedCapacity="INDEXES",
+            ReturnItemCollectionMetrics="SIZE",
+        )
+        return response, response["ResponseMetadata"]["HTTPStatusCode"]
+
+
+dynamodb_client = _DynamoDBClient()
