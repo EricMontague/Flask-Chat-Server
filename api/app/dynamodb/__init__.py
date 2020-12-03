6902bd3ae29d7eadb9086195a42c6191c077a1b6
Eric Montague <eric.g.montague@gmail.com>
2020-12-03 00:11:08 -0500

    Added imports

---
+++ api/app/dynamodb/__init__.py
@@ -0,0 +1,5 @@
+"""This package contains modules related to working with DynamoDB."""
+
+
+from app.dynamodb.mapping import UpdateExpression
+from app.dynamodb.key_prefix import KeyPrefix
---
+++ api/app/repositories/__init__.py
@@ -0,0 +1,6 @@
+"""This package holds classes that implement the repository pattern
+and act as an abstraction over persitent storage.
+"""
+
+
+from app.repositories.dynamodb_repository import dynamodb_repository
