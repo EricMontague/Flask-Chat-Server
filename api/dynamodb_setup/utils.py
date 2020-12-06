0170dcbb4c77644bf11d9136016c14bdf94d4062
Eric Montague <eric.g.montague@gmail.com>
2020-12-06 01:01:56 -0500

    Added new attributes and GSis

--- api/dynamodb_setup/utils.py
+++ api/dynamodb_setup/utils.py
@@ -6,15 +6,13 @@ for Dynamo tables.
 import os
 import boto3
 from dynamodb_setup.global_secondary_indexes import (
-    COMMUNITIES_GSI,
-    CHAT_REQUESTS_GSI,
-    PRIVATE_CHATS_GSI,
-    GROUP_CHATS_GSI
+    INVERTED_GSI,
+    COMMUNITIES_BY_LOCATION_GSI,
+    COMMUNITIES_BY_TOPIC_GSI,
 )
 
 
 dynamodb = boto3.resource("dynamodb")
-
 
 
 def create_users_table():
@@ -22,18 +20,23 @@ def create_users_table():
     table = dynamodb.create_table(
         TableName="Users",
         KeySchema=[
-            {"AttributeName": "PartitionKey", "KeyType": "HASH"},
-            {"AttributeName": "SortKey", "KeyType": "RANGE"}
+            {"AttributeName": "PK", "KeyType": "HASH"},
+            {"AttributeName": "SK", "KeyType": "RANGE"},
         ],
         AttributeDefinitions=[
-            {"AttributeName": "PartitionKey", "AttributeType": "S"},
-            {"AttributeName": "SortKey", "AttributeType": "S"}
+            {"AttributeName": "PK", "AttributeType": "S"},
+            {"AttributeName": "SK", "AttributeType": "S"},
+            {"AttributeName": "country", "AttributeType": "S"},
+            {"AttributeName": "state_city", "AttributeType": "S"},
+            {"AttributeName": "topic", "AttributeType": "S"},
+            {"AttributeName": "community_id", "AttributeType": "S"},
+            {"AttributeName": "request_status_datetime", "AttributeType": "S"},
+            {"AttributeName": "group_chat_id", "AttributeType": "S"},
         ],
         GlobalSecondaryIndexes=[
-            COMMUNITIES_GSI,
-            CHAT_REQUESTS_GSI,
-            PRIVATE_CHATS_GSI,
-            GROUP_CHATS_GSI
+            INVERTED_GSI,
+            COMMUNITIES_BY_LOCATION_GSI,
+            COMMUNITIES_BY_TOPIC_GSI,
         ],
         ProvisionedThroughput={
             "ReadCapacityUnits": os.environ.get("AWS_DYNAMODB_RCU", 5),
