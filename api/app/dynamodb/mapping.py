ddb4c6e039d95215e278970d079db9fba818eb53
Eric Montague <eric.g.montague@gmail.com>
2020-12-07 23:38:12 -0500

    Deleted uncessary mapper classes

--- api/app/dynamodb/mapping.py
+++ api/app/dynamodb/mapping.py
@@ -5,84 +5,12 @@ models to DynamoDB items.
 
 from enum import Enum
 from abc import ABC, abstractmethod
+from app.models import User
 
 
-class BaseItemMapper(ABC):
-    """Abstract base class for all DynamoDB item mappers."""
-
-    fields_to_exclude = ()
-
-    def load(self, item):
-        """Convert an item from DynamoDB into a model."""
-        pass
-
-    @classmethod
-    def dump(cls, model):
-        """Convert a model into an item to be inserted into
-        DynamoDB.
-        """
-        pass
-
-
-class UserItemMapper(BaseItemMapper):
-    """Class to map user models to dynamodb items
-    and vice versa.
-    """
-
-    fields_to_exclude = (
-        "notifications",
-        "pending_chat_requests",
-        "private_chats",
-        "group_chats",
-        "communities",
-    )
-
-    @classmethod
-    def load(cls, item):
-        """Convert an item from DynamoDB into a model."""
-        # user = User(**item["profile_data"])
-        # cls._add_notifications(user, item["notifications"])
-        # self._add_communities(user, item["communities"])
-        # self._add_group_chats(user, item["group_chats"])
-        # self._add_private_chats(user, item["private_chats"])
-        # self._add_chat_requests(user, item["chat_requests"])
-        # # location = Location(**item.get("location"))
-        # # avatar = Image(**item.get("avatar"))
-        # # cover_photo = Image(**item.get("cover_photo"))
-        # # role = Image(**item.get("role"))
-        # return user
-        return None
-
-    # def _add_notifications(self, user, notifications):
-    #     # need to sort notifications first
-    #     user._notifications = {
-    #         notification_id: Notification(**notification)
-    #         for notification_id, notification in notifications.items()
-    #     }
-
-    # def _add_communities(self, user, communities):
-    #     user._communities = {
-    #         community_id: Community(**community)
-    #         for community_id, community in communities.items()
-    #     }
-
-    # def _add_group_chats(self, user, group_chats):
-    #     user._group_chats = {
-    #         group_chat_id: GroupChat(**group_chat)
-    #         for group_chat_id, group_chat in group_chat.items()
-    #     }
-
-    # def _add_private_chats(self, user, private_chats):
-    #     user._private_chats = {
-    #         private_chat_id: PrivateChat(**private_chat)
-    #         for private_chat_id, private_chat in private_chats.items()
-    #     }
-
-    # def _add_chat_requests(self, user, chat_requests):
-    #     user._pending_chat_requests = {
-    #         chat_request_id: ChatRequest(**chat_request)
-    #         for chat_request_id, chat_request in chat_requests.items()
-    #     }
+def create_user_from_item(user_item):
+    """Create and return a user model from a DynamoDB item."""
+    pass
 
 
 class UpdateAction(Enum):
@@ -101,6 +29,7 @@ class UpdateExpression:
         self._build_expression(action, attributes_to_values)
 
     def _build_expression(self, action, attributes_to_values):
+        """Build an update expression for DynamoDB."""
         expression = []
         self.original_attribute_names = []
         self.attribute_name_placeholders = {}
@@ -108,18 +37,24 @@ class UpdateExpression:
         for attribute_name in attributes_to_values:
             attribute_name_lower = attribute_name.lower()
             self.original_attribute_names.append(attribute_name_lower)
-            nested_attributes = attribute_name_lower.split(
-                "."
-            )  # handle nested attributes
+
+             # handle nested attributes
+            nested_attributes = attribute_name_lower.split(".") 
             name_alias = ".#".join(nested_attributes)
             expression.append(
                 f"#{name_alias} = :{attribute_name_lower.replace('.', '')}"
             )
+
             # Map aliases for nested attibutes to actual attributes
             for attribute in nested_attributes:
                 self.attribute_name_placeholders[f"#{attribute}"] = attribute
+
+            # Map aliases for attribute values
             self.attribute_value_placeholders[
                 f":{attribute_name_lower.replace('.', '')}"
             ] = attributes_to_values[attribute_name]
         self.expression = action.name + " " + ", ".join(expression)
+
+    
+
 
