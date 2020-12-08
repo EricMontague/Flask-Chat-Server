16614ee033dd0f4122f2deb69bd6b8ed263a3ad9
Eric Montague <eric.g.montague@gmail.com>
2020-12-07 23:37:46 -0500

    Refactored and added new methods for updating and deleting a user

--- api/app/clients/dynamodb_client.py
+++ api/app/clients/dynamodb_client.py
@@ -5,13 +5,15 @@ the boto3 library's DynamoDB resource.
 
 import os
 import logging
+import logging.config
 import boto3
-from boto3.dynamodb.conditions import Key, Attr, And
+from pprint import pprint
+from app.dynamodb import UpdateExpression, UpdateAction
 from botocore.exceptions import ClientError
 
 
-logging.basicConfig(level=logging.INFO)
-logger = logging.getLogger(__name__)
+logging.config.fileConfig("logging.conf")
+logger = logging.getLogger("dynamoDBClient")
 
 
 class _DynamoDBClient:
@@ -23,71 +25,52 @@ class _DynamoDBClient:
         self._dynamodb = boto3.client("dynamodb")
         self._table_name = os.environ.get("AWS_DYNAMODB_TABLE_NAME", "ChatAppTable")
 
-    def create_user(self, data):
+    def create_user(self, items):
         """Add a new user item to DynamoDB."""
-        parameters = self._build_create_user_parameters(data)
+        logger.info("Adding new user to DynamoDB")
+        parameters = self._build_create_user_parameters(items)
         try:
             return self._execute_transact_write(parameters)
         except ClientError as err:
-            logging.error(f"{err.response['Error']['Code']}")
-            logging.error(f"{err.response['Error']['Message']}")
-
+            logger.error(f"{err.response['Error']['Code']}")
+            logger.error(f"{err.response['Error']['Message']}")
+            
             error_message = "Could not add user"
             if err.response["Error"]["Code"] == "TransactionCanceledException":
-                if err.response["CancellationReasons"][1]["Code"] == "ConditionalCheckFailed":
+                if (
+                    err.response["CancellationReasons"][1]["Code"]
+                    == "ConditionalCheckFailed"
+                ):
                     error_message = "A user with this email already exists"
-                elif err.response["CancellationReasons"][2]["Code"] == "ConditionalCheckFailed":
+                elif (
+                    err.response["CancellationReasons"][2]["Code"]
+                    == "ConditionalCheckFailed"
+                ):
                     error_message = "A user with this username already exists"
             return {"error": error_message}
-    
-    def _build_create_user_parameters(self, data):
-        """Return the parameters necessary to create a user in DynamoDB."""
-        parameters = {
-            "transact_items": [
-                {
-                    "Put": {
-                        "Item": data["user_item"],
-                        "TableName": self._table_name,
-                        "ConditionExpression": "attribute_not_exists(PK)"
-                    }
-                },
-                {
-                    "Put": {
-                        "Item": data["user_email"],
-                        "TableName": self._table_name,
-                        "ConditionExpression": "attribute_not_exists(email)"
-                    }
-                },
-                {
-                    "Put": {
-                        "Item": data["username"],
-                        "TableName": self._table_name,
-                        "ConditionExpression": "attribute_not_exists(username)"
-                    }
-                }
-            ]
-        }
-        return parameters
 
-    def get_item(self, primary_key):
+    def delete_user(self, keys):
+        """Delete a user item from DynamoDB."""
+        logger.info("Deleting a user from DynamoDB")
+        parameters = self._build_delete_user_parameters(keys)
+        try:
+            return self._execute_transact_write(parameters)
+        except ClientError as err:
+            logger.error(f"{err.response['Error']['Code']}")
+            logger.error(f"{err.response['Error']['Message']}")
+            error_message = "Could not delete user"
+            pprint(err.response)
+            return {"error": error_message}
+
+    def get_item(self, key):
         """Return a single item from DynamoDB. This method
         will return the item with all of its attributes.
         """
-        response = self._dynamodb.get_item(
-            TableName=self._table_name,
-            Key=primary_key,
-        )
-        return response
-
-    def _execute_transact_write(self, parameters):
-        """Write the given items into DynamoDB as part of a transaction."""
-        response = self._dynamodb.transact_write_items(
-            TransactItems=parameters["transact_items"]
-        )
+        response = self._dynamodb.get_item(TableName=self._table_name, Key=key,)
         return response
 
     def query(self, partition_key, sort_key):
-        """Return the results of a DynamoDB query to the users table."""
+        """Return the results of a DynamoDB query on the table."""
         response = self._dynamodb.query(
             Select="ALL_ATTRIBUTES",
             KeyConditionExpression=Key("PK").eq(partition_key) & Key("SK").eq(sort_key),
@@ -95,16 +78,35 @@ class _DynamoDBClient:
         return response, response["ResponseMetadata"]["HTTPStatusCode"]
 
     def put_item(self, user_item):
-        """Replace an entire item in the users table or create a new item
+        """Replace an entire item in the table or create a new item
         if it exists. Returns the response from DynamoDB afterwards
         """
         response = self._dynamodb.put_item(Item=user_item)
         return response, response["ResponseMetadata"]["HTTPStatusCode"]
 
-    def update_item(self, partition_key, sort_key, expression):
-        """Update an item in the users table and return the response."""
+    def update_user(self, keys, attributes):
+        """Update a user item in the table """
+        # Special cases are changing a user's email, or username
+        if "email" in attributes or "username" in attributes or "role" in attributes:
+            return self._update_with_transaction(keys, attributes)
+        return self._update_without_transaction(keys["user"], attributes)
+
+    def _update_with_transaction(self, keys, attributes):
+        parameters = self._build_update_user_parameters(keys, attributes)
+        try:
+            return self._execute_transact_write(parameters)
+        except ClientError as err:
+            logger.error(f"{err.response['Error']['Code']}")
+            logger.error(f"{err.response['Error']['Message']}")
+
+            error_message = "Could not update user"
+            return {"error": error_message}
+    
+    def _update_without_transaction(self, key, attributes):
+        expression = UpdateExpression(UpdateAction.SET, attributes)
         response = self._dynamodb.update_item(
-            Key={"PK": partition_key, "SK": sort_key},
+            TableName=self._table_name,
+            Key=key,
             UpdateExpression=expression.expression,
             ConditionExpression=self._build_condition_expression(
                 expression.original_attribute_names
@@ -112,25 +114,87 @@ class _DynamoDBClient:
             ExpressionAttributeNames=expression.attribute_name_placeholders,
             ExpressionAttributeValues=expression.attribute_value_placeholders,
         )
+        return response
+
+    def delete_item(self, key):
+        """Delete an item from DynamoDB and return the response."""
+        response = self._dynamodb.delete_item(
+            TableName=self._table_name, Key=key
+        )
         return response, response["ResponseMetadata"]["HTTPStatusCode"]
+
+    def _execute_transact_write(self, parameters):
+        """Write the given items into DynamoDB as part of a transaction."""
+        response = self._dynamodb.transact_write_items(
+            TransactItems=parameters
+        )
+        return response
 
     def _build_condition_expression(self, attribute_names):
         """Build an expression for conditionally updating one or more fields."""
-        if len(attribute_names) > 1:
-            condition_expression = [Attr(name).exists() for name in attribute_names]
-            return And(*condition_expression)
-        # When updating a single attribute, the AND condition can't be used, else boto3 throws
-        # an error, so single attribute updates need to be handled differently
+        condition_expression = [f"attribute_exists({name})" for name in attribute_names]
+        return " AND ".join(condition_expression)
+        
+    # May move these _build_* methods into another module and make them functions if this
+    # module becomes too large
+    def _build_create_user_parameters(self, items):
+        """Return the parameters necessary to create a user in a transaction."""
+        parameters = [
+            {
+                "Put": {
+                    "Item": items[item],
+                    "TableName": self._table_name,
+                    "ConditionExpression": "attribute_not_exists(PK)",
+                }
+            }
+            for item in items
+        ]
+        return parameters
 
-        return Attr(attribute_names[0]).exists()
+    def _build_update_user_parameters(self, keys, attributes):
+        """Return the parameters necessary for updating a user in a transaction."""
+        expressions = [
+            UpdateExpression(UpdateAction.SET, attributes[key])
+            for key in keys
+        ]
+        parameters = [
+            {
+                "Update": {
+                    "Key": keys[key],
+                    "TableName": self._table_name,
+                    "UpdateExpression": expressions[key],
+                    "ConditionExpression": "attribute_exists(PK)"
+                }
+            }
+            for key in keys
+        ]
+        if "role" in attributes:
+            parameters.append(
+                {
+                    "ConditionCheck": {
+                        "TableName": self._table_name,
+                        "Key": key["user"],
+                        "ConditionExpression": f'role.name = {attributes["role"]["name"]["S"]}'
+                    }
+                }
+            )
+        return parameters  
 
-    def delete_item(self, primary_key):
-        """Delete an item from DynamoDB and return the response."""
-        response = self._dynamodb.delete_item(
-            TableName=self._table_name,
-            Key=primary_key
-        )
-        return response, response["ResponseMetadata"]["HTTPStatusCode"]
+    def _build_delete_user_parameters(self, keys):
+        """Return the parameters necessary for deleting a user in a transaction."""
+        parameters = [
+            {
+                "Delete": {
+                    "Key": keys[key],
+                    "TableName": self._table_name,
+                    "ConditionExpression": "attribute_exists(PK)"
+                }
+            }
+            for key in keys
+        ]
+        return parameters
+
+    
 
 
 dynamodb_client = _DynamoDBClient()
--- api/app/repositories/dynamodb_repository.py
+++ api/app/repositories/dynamodb_repository.py
@@ -3,14 +3,7 @@
 
 from http import HTTPStatus
 from app.clients import dynamodb_client
-from app.dynamodb import UpdateExpression, PrimaryKeyPrefix, UpdateAction
-from app.models import (
-    User, 
-    UserEmail,
-    Username,
-    CommunityMembership, 
-    Community
-)
+from app.models import User, UserEmail, Username, CommunityMembership, Community
 
 
 class _DynamoDBRepository:
@@ -22,72 +15,60 @@ class _DynamoDBRepository:
     def get_user(self, user_id):
         """Return a user from DynamoDB by id."""
         response = self._dynamodb_client.get_item(User.key(user_id))
+        if "Item" not in response:
+            return None
         return response
 
     def add_user(self, user):
         """Add a new user to DynamoDB."""
         user_email = UserEmail(user.id, user.email)
         username = Username(user.id, user.username)
-        data = {
-            "user_item": user.to_item(),
+        items = {
+            "user": user.to_item(),
             "user_email": user_email.to_item(),
-            "username": username.to_item()
+            "username": username.to_item(),
         }
-        response = self._dynamodb_client.create_user(data)
+        response = self._dynamodb_client.create_user(items)
         if "error" in response:
             # should I raise an exception here?
             print("Error!")
         return response
 
-    def get_community(self, community_id):
-        """Return a community from DynamoDB by id."""
-
-        response, status = self._dynamodb_client.query(
-            PrimaryKeyPrefix.COMMUNITY + community_id,
-            PrimaryKeyPrefix.COMMUNITY + community_id,
-        )
-        if status != 200:
-            # This will later be where logging happens
-            print(f"Non-200 status code! {status}")
-        if "Items" not in response:
-            return (None, None)
-        return response, status
-
-    def create_community(self, community):
-        """Add a new community to DynamoDB."""
-        membership = CommunityMembership(community._founder.id, community.id)
-        responses = self._dynamodb_client.batch_create(
-            [community.to_item(), membership.to_item()]
-        )
-        return responses
-
-    def create_community_group_chat(self, group_chat, community_group_chat):
-        """Add a new community group chat to DynamoDB."""
-        responses = self._dynamodb_client.batch_create(
-            [group_chat.to_item(), community_group_chat.to_item()]
-        )
-        return responses
-
-    def update_user(self, user_id, attributes_to_values):
+    def update_user(self, user, attributes_to_update):
         """Update a user item in DynamoDB."""
-        expression = UpdateExpression(UpdateAction.SET, attributes_to_values)
-        response, status = self._dynamodb_client.update_item(
-            PrimaryKeyPrefix.USER + user_id, PrimaryKeyPrefix.USER + user_id, expression
-        )
-        if status != 200:
-            # This will later be turned into a log statement
-            print(f"Non-200 status code! {status}")
-        return response, status
+        keys, attributes = self._build_attributes_and_keys(user, attributes_to_update)
+        response = self._dynamodb_client.update_user(keys, attributes)
+        return response
 
-    def delete_user(self, user_id):
-        """Delete a user from DynamoDB."""
-        response, status = self._dynamodb_client.delete_item(
-            PrimaryKeyPrefix.USER + user_id, PrimaryKeyPrefix.USER + user_id
-        )
-        if status != 200:
-            # This will later be turned into a log statement
-            print(f"Non-200 status code! {status}")
-        return response, status
+    def remove_user(self, user):
+        """Delete a user item from DynamoDB."""
+        keys = {
+            "user": User.key(user.id),
+            "username": Username.key(user.username),
+            "user_email": UserEmail.key(user.email),
+        }
+        response = self._dynamodb_client.delete_user(keys)
+        return response
+
+    def _build_attributes_and_keys(self, user, attributes_to_update):
+        filtered_attributes = self._filter_attributes(user.to_item(), attributes_to_update)
+        final_attributes = {}
+        keys = {"user": User.key(user.id)}
+        if "email" in filtered_attributes:
+            keys["user_email"] = UserEmail.key(user.email)
+            final_attributes["user_meail"] = filtered_attributes.pop("email")
+        if "username" in filtered_attributes:
+            keys["username"] = Username.key(user.username)
+            final_attributes["username"] = filtered_attributes.pop("username")
+        final_attributes["user"] = filtered_attributes
+        return keys, final_attributes
+
+    def _filter_attributes(self, item, attributes):
+        """Return a dictionary containing a subset of the
+        attributes of the given DynamoDB item.
+        """
+        filtered_attributes = {attribute: item[attribute] for attribute in attributes}
+        return filtered_attributes
 
 
 dynamodb_repository = _DynamoDBRepository(dynamodb_client)
