a362ccd4c17909b6c556c8bf70e86ce34fc80cc9
Eric Montague <eric.g.montague@gmail.com>
2020-12-10 00:42:41 -0500

    Created decorators to handle serializing and deserializing requests and responses

---
+++ api/app/helpers/__init__.py
@@ -0,0 +1,6 @@
+"""This package contains various helper functions to be used
+throughout the application.
+"""
+
+
+from app.helpers.decorators import handle_request, handle_response
---
+++ api/app/helpers/decorators.py
@@ -0,0 +1,47 @@
+"""This module contains decorators to be used through the application."""
+
+
+import functools
+from http import HTTPStatus
+from flask import request
+from marshmallow import ValidationError
+
+
+def handle_request(schema):
+    """Decorator to handle deserializing the JSON body
+    of requests to API routes.
+    """
+    
+    def decorator(view_func):
+
+        @functools.wraps
+        def wrapper(*args, **kwargs):
+            request_data = request.json
+            if not request_data:
+                return {"message": "Missing JSON Body in request"}, HTTPStatus.BAD_REQUEST
+            try:
+                result = schema.load(request_data)
+            except ValidationError as err:
+                return {"error": err}, HTTPStatus.UNPROCESSABLE_ENTITY
+            return view_func(result, *args, **kwargs)
+        
+        return wrapper
+    
+    return decorator
+
+
+def handle_response(schema):
+    """Decorator to handle serializing models to JSON
+    responses for API routes.
+    """
+
+    def decorator(view_func):
+
+        @functools.wraps
+        def wrapper(*args, **kwargs):
+            model_or_models, http_status_code = view_func(*args, **kwargs)
+            return schema.dump(model_or_models), http_status_code
+        
+        return wrapper
+    
+    return decorator
