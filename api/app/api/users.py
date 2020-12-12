64cf7b8a9dd1c4ee437217aa63a2a9848c5554b4
Eric Montague <eric.g.montague@gmail.com>
2020-12-12 00:57:26 -0500

    Finished the get user and create user endpoints

--- api/app/api/users.py
+++ api/app/api/users.py
@@ -1,21 +1,30 @@
 """This module contains view functions for accessing user resources."""
 
 
+from http import HTTPStatus
+from flask import current_app
 from app.api import api
 from app.helpers import handle_request, handle_response
 from app.schemas import UserSchema
 from app.repositories import dynamodb_repository
+from app.models.user_factory import UserFactory
+from pprint import pprint
 
 
+@api.route("/users/<user_id>")
 @handle_response(UserSchema())
-@api.route("/users/<int:user_id>")
 def get_user(user_id):
     """Return a single user by id."""
-    return dynamodb_repository.get_user(user_id)
+    return dynamodb_repository.get_user(user_id), HTTPStatus.OK
 
 
-@handle_response(UserSchema(many=True))
-@api.route("/users")
-def get_users():
-    """Return a collection of user resources."""
-    return dynamodb_repository.get_users()
+@api.route("/users", methods=["POST"])
+@handle_request(UserSchema())
+@handle_response(UserSchema())
+def create_user(user_data):
+    """Create a new user resource."""
+    user = UserFactory.create_user(user_data)
+    response = dynamodb_repository.add_user(user)
+    pprint(response)
+    return user, HTTPStatus.CREATED
+
