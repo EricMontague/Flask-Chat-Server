8b2d4e78fd8f134d962b91922b557ea0fe8a1f97
Eric Montague <eric.g.montague@gmail.com>
2020-12-12 00:53:21 -0500

    Corrected minor bugs with the request and response decorators as well as started on a query param decorator

--- api/app/helpers/decorators.py
+++ api/app/helpers/decorators.py
@@ -11,22 +11,24 @@ def handle_request(schema):
     """Decorator to handle deserializing the JSON body
     of requests to API routes.
     """
-    
-    def decorator(view_func):
 
-        @functools.wraps
+    def decorator(view_func):
+        @functools.wraps(view_func)
         def wrapper(*args, **kwargs):
             request_data = request.json
             if not request_data:
-                return {"message": "Missing JSON Body in request"}, HTTPStatus.BAD_REQUEST
+                return (
+                    {"message": "Missing JSON Body in request"},
+                    HTTPStatus.BAD_REQUEST,
+                )
             try:
-                result = schema.load(request_data)
+                model_dict = schema.load(request_data)
             except ValidationError as err:
-                return {"error": err}, HTTPStatus.UNPROCESSABLE_ENTITY
-            return view_func(result, *args, **kwargs)
-        
+                return {"errors": err.messages}, HTTPStatus.UNPROCESSABLE_ENTITY
+            return view_func(model_dict, *args, **kwargs)
+
         return wrapper
-    
+
     return decorator
 
 
@@ -36,12 +38,25 @@ def handle_response(schema):
     """
 
     def decorator(view_func):
-
-        @functools.wraps
+        @functools.wraps(view_func)
         def wrapper(*args, **kwargs):
             model_or_models, http_status_code = view_func(*args, **kwargs)
             return schema.dumps(model_or_models), http_status_code
-        
+
         return wrapper
-    
+
     return decorator
+
+
+def handle_query_params(view_func):
+    """Decorator to handle parsing query parameters sent
+    during a request to the api. It handles pagination, filtering, and sorting 
+    for collections. The expected response from the decorated route is a
+    an object that implements the AbstractDatabaseRepository interface."""
+
+    @functools.wraps(view_func)
+    def wrapper(*args, **kwargs):
+        params = request.args
+        return view_func(params, *args, **kwargs)
+
+    return wrapper
