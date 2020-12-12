61a65a54001c380bb5b080442b83fe738ea1eca8
Eric Montague <eric.g.montague@gmail.com>
2020-12-12 00:51:31 -0500

    Added a results per page value

--- api/config.py
+++ api/config.py
@@ -3,18 +3,20 @@
 
 import os
 
+
 class BaseConfig:
     """Base configuration class for the application."""
-    
+
     # Make sure this is set for production
     SECRET_KEY = os.environ.get("SECRET_KEY", "hard to guess string")
-
+    RESULTS_PER_PAGE = 20
 
 
 class DevelopmentConfig(BaseConfig):
     """Class to be used for configuring the application in
     development.
     """
+
     DEBUG = True
 
 
@@ -22,6 +24,7 @@ class TestingConfig(BaseConfig):
     """Class to be used for configuring the application in
     testing.
     """
+
     TESTING = True
 
 
@@ -29,11 +32,12 @@ class ProductionConfig(BaseConfig):
     """Class to be used for configuring the application in
     production.
     """
+
     pass
 
 
 CONFIG_MAPPER = {
     "development": DevelopmentConfig,
     "testing": TestingConfig,
-    "production": ProductionConfig
+    "production": ProductionConfig,
 }
