7f5a394ef74b8ab9ca08bd83ce5413e56cc74202
Eric Montague <eric.g.montague@gmail.com>
2020-12-12 00:58:15 -0500

    Imported new models

--- api/app/models/__init__.py
+++ api/app/models/__init__.py
@@ -3,10 +3,10 @@
 
 from app.models.chat_request import ChatRequest, ChatRequestStatus
 from app.models.chat import PrivateChat, GroupChat
-from app.models.community import Community, CommunityTopic
+from app.models.community import Community, CommunityTopic, CommunityMembership, CommunityPermission
 from app.models.image import Image, ImageType
 from app.models.message import Message, Reaction
 from app.models.notification import Notification, NotificationType
 from app.models.role import Role, RolePermission, RoleName
-from app.models.user import User
+from app.models.user import User, UserEmail, Username
 from app.models.location import Location
