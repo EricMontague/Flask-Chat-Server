b48c21014646bb42d42cf40623b3551b90b24ea7
Eric Montague <eric.g.montague@gmail.com>
2020-12-12 23:32:00 -0500

    Finished implementing basic user routes

--- api/app/api/users.py
+++ api/app/api/users.py
@@ -2,11 +2,12 @@
 
 
 from http import HTTPStatus
-from flask import current_app
+from flask import current_app, url_for
 from app.api import api
 from app.helpers import handle_request, handle_response
 from app.schemas import UserSchema
 from app.repositories import dynamodb_repository
+from app.repositories.exceptions import DatabaseException
 from app.models.user_factory import UserFactory
 from pprint import pprint
 
@@ -15,7 +16,10 @@ from pprint import pprint
 @handle_response(UserSchema())
 def get_user(user_id):
     """Return a single user by id."""
-    return dynamodb_repository.get_user(user_id), HTTPStatus.OK
+    user = dynamodb_repository.get_user(user_id)
+    if not user:
+        return {"error": "User not found"}, HTTPStatus.NOT_FOUND
+    return user, HTTPStatus.OK
 
 
 @api.route("/users", methods=["POST"])
@@ -24,7 +28,33 @@ def get_user(user_id):
 def create_user(user_data):
     """Create a new user resource."""
     user = UserFactory.create_user(user_data)
-    response = dynamodb_repository.add_user(user)
-    pprint(response)
-    return user, HTTPStatus.CREATED
+    try:
+        dynamodb_repository.add_user(user)
+    except DatabaseException as err:
+        return {"error": str(err)}, HTTPStatus.BAD_REQUEST
+    headers = {"Location": url_for("api.get_user", user_id=user.id)}
+    return user, HTTPStatus.CREATED, headers
 
+
+@api.route("/users/<user_id>", methods=["PUT"])
+@handle_request(UserSchema(partial=["password"]))
+@handle_response(None)
+def update_user(user_data):
+    """Replace a user resource."""
+    user = UserFactory.create_user(user_data)
+    try:
+        dynamodb_repository.add_user(user)
+    except DatabaseException as err:
+        return {"error": str(err)}, HTTPStatus.BAD_REQUEST
+    return {}, HTTPStatus.NO_CONTENT
+
+
+@api.route("/users/<user_id>", methods=["DELETE"])
+@handle_response(None)
+def delete_user(user_id):
+    """Delete a user resource."""
+    user = dynamodb_repository.get_user(user_id)
+    if not user:
+        return {"error": "User not found"}, HTTPStatus.NOT_FOUND
+    dynamodb_repository.remove_user(user)
+    return {}, HTTPStatus.NO_CONTENT
