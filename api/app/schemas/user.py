c6cbd418843901e65eed7cf7295c88215488c25a
Eric Montague <eric.g.montague@gmail.com>
2020-12-12 23:34:06 -0500

    Added pre and post load decoratored methods

--- api/app/schemas/user.py
+++ api/app/schemas/user.py
@@ -6,7 +6,7 @@ User models.
 from app.extensions import ma
 from app.schemas.location import LocationSchema
 from app.schemas.image import ImageSchema
-from marshmallow import validate
+from marshmallow import validate, pre_load, post_load
 
 
 
@@ -23,15 +23,29 @@ class UserSchema(ma.Schema):
 
     _id = ma.UUID(data_key="id") # will exclude this on POST requests: schema.load(partial=("id",))
     username = ma.Str(required=True, validate=validate.Length(min=1, max=32))
-    password = ma.Str(required=True, load_only=True)
     name = ma.Str(required=True, validate=validate.Length(min=1, max=32))
     email = ma.Email(required=True, validate=validate.Length(min=1, max=32))
     _created_at = ma.DateTime(data_key="joined_on")  # defaults to ISO 8601
     last_seen_at = ma.DateTime()  # defaults to ISO 8601
+    password = ma.Str(required=True, load_only=True)
     bio = ma.Str(validate=validate.Length(max=280))
+    resource_type = ma.Str(default="User", dump_only=True)
+
     location = ma.Nested(LocationSchema, required=True)
     avatar = ma.Nested(ImageSchema)
     cover_photo = ma.Nested(ImageSchema)
-    type_ = ma.Str(default="User", dump_only=True, data_key="type")
-
     
+    @pre_load
+    def strip_unwanted_fields(self, data, many, **kwargs):
+        """Remove unwanted fields from the input data before deserialization."""
+        unwanted_fields = ["resource_type"]
+        for field in unwanted_fields:
+            if field in data:
+                data.pop(field)
+        return data
+
+    @post_load
+    def convert_uuid_to_hex(self, data, many, **kwargs):
+        """Convert the user's id to its hex value."""
+        data["_id"] = data["_id"].hex
+        return data
