57d252e20929224055fc0c25f1c30071c7e0adb9
Eric Montague <eric.g.montague@gmail.com>
2020-12-14 00:38:46 -0500

    Finished implementing the get users and update user routes

--- api/app/api/users.py
+++ api/app/api/users.py
@@ -5,11 +5,21 @@ from http import HTTPStatus
 from flask import current_app, url_for
 from app.api import api
 from app.helpers import handle_request, handle_response
-from app.schemas import UserSchema
+from app.schemas import UserSchema, PaginationSchema
 from app.repositories import dynamodb_repository
 from app.repositories.exceptions import DatabaseException
 from app.models.user_factory import UserFactory
-from pprint import pprint
+
+
+@api.route("/users")
+@handle_request(PaginationSchema())
+@handle_response(UserSchema(many=True))
+def get_users(pagination):
+    """Return a list of user resources."""
+    per_page = pagination.get("per_page", current_app.config["RESULTS_PER_PAGE"])
+    cursor = pagination.get("next_cursor")
+    results = dynamodb_repository.get_users(per_page, cursor)
+    return results, HTTPStatus.OK
 
 
 @api.route("/users/<user_id>")
@@ -39,13 +49,12 @@ def create_user(user_data):
 @api.route("/users/<user_id>", methods=["PUT"])
 @handle_request(UserSchema(partial=["password"]))
 @handle_response(None)
-def update_user(user_data):
+def update_user(user_data, user_id):
     """Replace a user resource."""
-    user = UserFactory.create_user(user_data)
-    try:
-        dynamodb_repository.add_user(user)
-    except DatabaseException as err:
-        return {"error": str(err)}, HTTPStatus.BAD_REQUEST
+    user = dynamodb_repository.get_user(user_id)
+    if not user:
+        return {"error": "User not found"}, HTTPStatus.NOT_FOUND
+    dynamodb_repository.update_user(user, user_data)
     return {}, HTTPStatus.NO_CONTENT
 
 
@@ -58,3 +67,5 @@ def delete_user(user_id):
         return {"error": "User not found"}, HTTPStatus.NOT_FOUND
     dynamodb_repository.remove_user(user)
     return {}, HTTPStatus.NO_CONTENT
+
+
