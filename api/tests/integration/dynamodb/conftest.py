b41a699881b23ff66ac1a55e6e89c8eceaf1712e
Eric Montague <eric.g.montague@gmail.com>
2020-12-21 01:04:28 -0500

    Added more tests for the mapper class

--- api/tests/integration/dynamodb/conftest.py
+++ api/tests/integration/dynamodb/conftest.py
@@ -5,29 +5,83 @@ code that interacts with DynamoDB.
 
 import pytest
 from app.dynamodb.mapper import ModelMapper
+from app.dynamodb.model_mappers import LocationMapper
+from app.models import Location
+from enum import Enum
 
 
+class TestEnum(Enum):
+    RED = 1
+    BLUE = 2
+    YELLOW = 3
 
-class FakeModel:
+
+class TestModel:
     """Class to be used in serialization and deserialization tests."""
 
-    pass
+    def __init__(self):
+        self.id = "wqtoqin"
+        self.age = 25
+        self.has_paid = True
+        self.role = None
+        self.string_set = {"A", "B", "C"}
+        self.num_set = {2, 3, 4, 5}
+        self.test_enum = TestEnum.RED
+        self.enum_set = {TestEnum.BLUE, TestEnum.YELLOW}
+        self.enum_list = [TestEnum.RED, TestEnum.YELLOW]
+        self.locations_list = [Location("New York", "New York", "US"), Location("Philadelphia", "PA", "US")]
+        self.locations_dict = {
+            "New York": Location("New York", "New York", "US"),
+            "Philadelphia": Location("Philadelphia", "PA", "US")
+        }
 
 
-class FakeMapper(ModelMapper):
+
+class TestMapper(ModelMapper):
     """Class to be used to run tests serializing and deserializing a 
     variety of different types of attributes that could be present on a model.
     """
-    pass
+    
+    class Meta:
+        model = TestModel
+        fields = (
+            "id", 
+            "age", 
+            "has_paid", 
+            "role", 
+            "string_set", 
+            "num_set",
+            "test_enum",
+            "enum_set",
+            "enum_list",
+            "locations_list",
+            "locations_dict",
+        )
+        partition_key_attribute = "id"
+        sort_key_attribute = "id"
+        type_ = "Test"
+        enum_attribute = "value"
+
+    ENUMS = {"test_enum": TestEnum}
+    NESTED_MAPPERS = {
+        "locations_list": LocationMapper(ignore_partition_key=True),
+        "locations_dict": LocationMapper(ignore_partition_key=True)
+    }
 
 
 @pytest.fixture
-def fake_mapper():
-    """Return an instance of a FakeMapper."""
-    return FakeMapper()
+def test_mapper():
+    """Return an instance of a TestMapper."""
+    return TestMapper()
 
 
 @pytest.fixture
-def fake_model:
-    """Return an instance of a FakeModel."""
-    return FakeModel()
+def test_model():
+    """Return an instance of a TesteModel."""
+    return TestModel()
+
+
+@pytest.fixture
+def test_key_prefix():
+    """Return the prefix for a test partition or sort key."""
+    return "TESTMODEL#"
--- api/tests/integration/dynamodb/test_mapper.py
+++ api/tests/integration/dynamodb/test_mapper.py
@@ -6,4 +6,141 @@ Note: These tests are dependent on the functionality of the boto3 libary
 
 
 import pytest
+from app.dynamodb.exceptions import ModelNotSetException
+
+
+def test_create_primary_key_without_sort_key_value(test_key_prefix, test_mapper):
+    """Test that a primary key is properly created if a sort key is not provided
+    on the mapper class.
+    """
+    partition_key = "12345"
+    primary_key = test_mapper.key(partition_key)
+    assert "PK" in primary_key
+    assert primary_key["PK"]["S"] == test_key_prefix + "12345"
+    assert "SK" not in primary_key
+
+
+def test_create_primary_key_with_sort_key_value(test_key_prefix, test_mapper):
+    """Test that a primary key is properly created if a sort key IS provided
+    on the mapper class.
+    """
+    partition_key = "12345"
+    sort_key = "6789"
+    primary_key = test_mapper.key(partition_key, sort_key)
+    assert "PK" in primary_key
+    assert primary_key["PK"]["S"] == test_key_prefix + "12345"
+    assert "SK" in primary_key
+    assert primary_key["SK"]["S"] == test_key_prefix + "6789"
+
+
+def test_construct_primary_key_with_partition_and_sort_key(test_key_prefix, test_model, test_mapper):
+    """Test of inner construct primary key method to make sure that if
+    a partition and sort key are provided, that a primary key is made with
+    both of them included.
+    """
+    partition_key = "12345"
+    sort_key = "6789"
+    primary_key = test_mapper._construct_primary_key(
+        test_model, partition_key, sort_key
+    )
+    assert "PK" in primary_key
+    assert primary_key["PK"]["S"] == test_key_prefix + "12345"
+    assert "SK" in primary_key
+    assert primary_key["SK"]["S"] == test_key_prefix + "6789"
+
+
+def test_construct_primary_key_without_partition_or_sort_key(test_key_prefix, test_model, test_mapper):
+    """Test of inner construct primary key method to make sure that if
+    a partition and sort key both not provided, that a primary key is made with
+    using the attributes provided on the options class
+    """
+    primary_key = test_mapper._construct_primary_key(test_model, None, None)
+    assert "PK" in primary_key
+    assert primary_key["PK"]["S"] == test_key_prefix+ test_model.id
+    assert "SK" in primary_key
+    assert primary_key["SK"]["S"] == test_key_prefix + test_model.id
+
+
+def test_construct_primary_key_with_partition_key_but_not_sort_key(test_key_prefix, test_model, test_mapper):
+    """Test of inner construct primary key method to make sure that if
+    a partition key is provided, but the sort key is not that a primary key is made with
+    using the passed in partition key value and the sort key from the options class
+    """
+    partition_key = "1234"
+    primary_key = test_mapper._construct_primary_key(test_model, partition_key, None)
+    assert "PK" in primary_key
+    assert primary_key["PK"]["S"] == test_key_prefix + "1234"
+    assert "SK" in primary_key
+    assert primary_key["SK"]["S"] == test_key_prefix + test_model.id
+
+
+def test_construct_primary_key_with_sort_key_but_not_partition_key(test_key_prefix, test_model, test_mapper):
+    """Test of inner construct primary key method to make sure that if
+    a sort key is provided, but the partition key is not that a primary key is made with
+    using the passed in sort key value and the partition key from the options class
+    """
+    sort_key = "5678"
+    primary_key = test_mapper._construct_primary_key(test_model, None, sort_key)
+    assert "PK" in primary_key
+    assert primary_key["PK"]["S"] == test_key_prefix + test_model.id
+    assert "SK" in primary_key
+    assert primary_key["SK"]["S"] == test_key_prefix + "5678"
+
+
+def test_serialize_ignore_partition_key():
+    pass
+
+
+def test_serialize_dont_ignore_partition_key():
+    pass
+
+
+def test_serialize_item_type_is_none():
+    pass
+
+
+def test_serialize_item_type_is_not_none():
+    pass
+
+
+def test_serialization(test_key_prefix, test_model, test_mapper):
+    """High level test to confirm that a broad range of types are handled and 
+    serialized properly.
+    """
+    item = test_mapper.serialize_from_model(test_model)
+    assert "PK" in item
+    assert "SK" in item
+    assert "type" in item
+    for attribute in test_model.__dict__:
+        assert attribute in item
+
+    assert item["PK"]["S"] == test_key_prefix + test_model.id
+    assert item["SK"]["S"] == test_key_prefix + test_model.id
+    assert item["type"]["S"] == "Test"
+    assert item["id"]["S"] == test_model.id
+    assert item["age"]["N"] == str(test_model.age)
+    assert item["has_paid"]["BOOL"] == test_model.has_paid
+    assert item["role"]["NULL"] == True
+    assert item["string_set"]["SS"] == list(test_model.string_set)
+    assert item["num_set"]["NS"] == list(map(str,test_model.num_set))
+    assert item["test_enum"]["N"] == str(test_model.test_enum.value)
+    assert item["enum_set"]["NS"] == list(map(lambda enum: str(enum.value), test_model.enum_set))
+    assert sorted(item["enum_list"]["L"]) == sorted(map(lambda enum: {"N": str(enum.value)}, test_model.enum_list))
+
+    assert len(item["locations_list"]["L"]) == len(test_model.locations_list)
+    assert item["locations_list"]["L"][0]["M"]["city"] == test_model.locations_list[0].city
+
+    assert len(item["locations_dict"]["L"]) == len(test_model.locations_dict)
+
+
+def test_deserialize_skip_attributes():
+    pass
+
+
+def test_deserialize_model_not_set_in_options_class_raises_exception():
+    pass
+
+
+def test_merge_items():
+    pass
 
