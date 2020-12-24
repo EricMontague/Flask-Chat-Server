c3ba96747c9e43d4b4768124d77ac30766aa2d3e
Eric Montague <eric.g.montague@gmail.com>
2020-12-24 00:55:14 -0500

    Added view functions for getting communities by topic and updating and deleting communities

--- api/app/api/communities.py
+++ api/app/api/communities.py
@@ -2,27 +2,41 @@
 
 
 from http import HTTPStatus
-from flask import url_for
+from flask import url_for, current_app
 from app.api import api
 from app.helpers.decorators import handle_request, handle_response
-from app.schemas import CommunitySchema
-from app.models.community_factory import CommunityFactory
+from app.schemas import CommunitySchema, UrlParamsSchema, CommunityTopicSchema
+from app.models.factories import CommunityFactory
 from app.repositories import dynamodb_repository
 from app.repositories.exceptions import DatabaseException
 
 
 @api.route("/communities")
-def get_communities():
-    pass
+@handle_request(UrlParamsSchema())
+@handle_response(CommunitySchema(many=True))
+def get_communities(url_params):
+    """Return a list of community resources."""
+    per_page = url_params.get("per_page", current_app.config["RESULTS_PER_PAGE"])
+    cursor = url_params.get("next_cursor", {})
+    results = dynamodb_repository.get_communities(per_page, cursor)
+    return results, HTTPStatus.OK
 
 
 @api.route("/communities")
-def get_communities_by_topic():
-    pass
+@handle_request(CommunityTopicSchema())
+@handle_response(CommunitySchema(many=True))
+def get_communities_by_topic(url_params):
+    """Return a list of communities resources by topic."""
+    per_page = url_params.get("per_page", current_app.config["RESULTS_PER_PAGE"])
+    cursor = url_params.get("next_cursor", {})
+    topic = url_params["topic"]
+    results = dynamodb_repository.get_communities_by_topic(per_page, topic.name, cursor)
+    return results, HTTPStatus.OK
 
 
 @api.route("/communities")
 def get_communities_by_location():
+    """Return a list of communities resources by location."""
     pass
 
 
@@ -55,23 +69,36 @@ def create_community(community_data):
     return (community, HTTPStatus.CREATED, headers)
 
 
-@api.route("/communites/<community_id>", methods=["PUT"])
+@api.route("/communities/<community_id>", methods=["PUT"])
+@handle_request(CommunitySchema())
+@handle_response(None)
 def update_community(community_data, community_id):
-    pass
+    """Replace a community resource."""
+    community = dynamodb_repository.get_community(community_id)
+    if not community:
+        return {"error": "Community not found"}, HTTPStatus.NOT_FOUND
+    dynamodb_repository.update_community(community, community_data)
+    return {}, HTTPStatus.NO_CONTENT
 
 
 @api.route("/communities/<community_id>", methods=["DELETE"])
+@handle_response(None)
 def delete_community(community_id):
-    pass
+    """Delete a community resource."""
+    community = dynamodb_repository.get_community(community_id)
+    if not community:
+        return {"error": "Community not found"}, HTTPStatus.NOT_FOUND
+    dynamodb_repository.remove_community(community)
+    return {}, HTTPStatus.NO_CONTENT
 
 
 # What method should this be?
-@api.route("/communities/<community_id>", methods=["PUT"])
+@api.route("/communities/<community_id>", methods=["POST"])
 def join_community():
     pass
 
 
 # What method should this be?
-@api.route("/communities/<community_id>", methods=["PUT"])
+@api.route("/communities/<community_id>", methods=["DELETE"])
 def leave_community():
     pass
