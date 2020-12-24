8ddc706704a202dfda69c76d7f5421491cf81490
Eric Montague <eric.g.montague@gmail.com>
2020-12-24 00:54:02 -0500

    Added function to update community model

--- api/app/models/update_models.py
+++ api/app/models/update_models.py
@@ -5,6 +5,7 @@ from copy import deepcopy
 from app.models.user import User
 from app.models.image import Image, ImageType
 from app.models.location import Location
+from app.models.community import CommunityTopic
 
 
 def update_user_model(old_user, updated_user_data):
@@ -18,3 +19,16 @@ def update_user_model(old_user, updated_user_data):
         else:
             setattr(updated_user, attribute, updated_user_data[attribute])
     return updated_user
+
+
+def update_community_model(old_community, updated_community_data):
+    """Populate the given community model with new data."""
+    updated_community = deepcopy(old_community)
+    for attribute in updated_community_data:
+        if attribute == "location":
+            updated_community.location.state = updated_community_data["location"]["state"]
+            updated_community.location.city = updated_community_data["location"]["city"]
+            updated_community.location.country = updated_community_data["location"]["country"]
+        else:
+            setattr(updated_community, attribute, updated_community_data[attribute])
+    return updated_community
