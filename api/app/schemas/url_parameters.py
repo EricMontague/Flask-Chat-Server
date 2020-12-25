6be77c0aaa939907e57bf2d5fbef97ea1d96ece5
Eric Montague <eric.g.montague@gmail.com>
2020-12-25 01:12:12 -0500

    Created CommunityUrlParamsSchema

--- api/app/schemas/url_parameters.py
+++ api/app/schemas/url_parameters.py
@@ -5,8 +5,9 @@ information from url parameters
 
 from app.extensions import ma
 from app.schemas.enum_field import EnumField
+from app.schemas.location import LocationSchema
 from app.models import CommunityTopic
-from marshmallow import EXCLUDE, validates_schema, ValidationError
+from marshmallow import EXCLUDE, validates_schema, ValidationError, validate
 
 
 class UrlParamsSchema(ma.Schema):
@@ -17,7 +18,7 @@ class UrlParamsSchema(ma.Schema):
 
     per_page = ma.Integer()
     next_cursor = ma.Str(data_key="next")
-    prev_cursor = ma.Str(data_key="prev")
+    # prev_cursor = ma.Str(data_key="prev")
 
     @validates_schema
     def validate_cursors(self, data, **kwargs):
@@ -31,10 +32,29 @@ class UrlParamsSchema(ma.Schema):
         return data
 
 
-class CommunityTopicSchema(UrlParamsSchema):
+class CommunityUrlParamsSchema(UrlParamsSchema):
     """Class to serialize and deserialize information from url parameters
-    for querying for communities by topic.
+    for community resources.
     """
 
-    topic = EnumField(CommunityTopic, required=True)
-    
+    topic = EnumField(CommunityTopic)
+    city = ma.Str(validate=validate.Length(min=1, max=64))
+    state = ma.Str(validate=validate.Length(min=1, max=32))
+    country = ma.Str(validate=validate.Length(min=1, max=32))
+
+    @validates_schema
+    def validate_location(self, data, **kwargs):
+        """Raise a ValidationError if the url parameters for a location are invalid."""
+        if "city" in data:
+            if "state" not in data or "country" not in data:
+                raise ValidationError(
+                    "Insufficient location details were provided and the given location"
+                    + " could not be determined"
+                )
+        if "state" in data and "country" not in data:
+            raise ValidationError(
+                    "Insufficient location details were provided and the given location"
+                    + " could not be determined"
+                )
+        return data
+
