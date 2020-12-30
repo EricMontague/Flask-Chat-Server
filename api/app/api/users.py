ded70905bb5c30cad9fba8b9a75e6320f5ed1271
Eric Montague <eric.g.montague@gmail.com>
2020-12-30 00:19:58 -0500

    Added routes for getting and updating user notifications

--- api/app/api/users.py
+++ api/app/api/users.py
@@ -12,7 +12,7 @@ from app.helpers import (
     upload_to_cdn,
     process_image
 )
-from app.schemas import UserSchema, UrlParamsSchema, CommunitySchema
+from app.schemas import UserSchema, UrlParamsSchema, CommunitySchema, NotificationSchema
 from app.repositories import dynamodb_repository, s3_repository
 from app.repositories.exceptions import DatabaseException, NotFoundException
 from app.models.factories import UserFactory
@@ -115,5 +115,40 @@ def upload_user_profile_photo(file, user_id):
         return {"error": "User not found"}, HTTPStatus.NOT_FOUND
     image_data = process_image(user.id, s3_repository, file, ImageType.USER_PROFILE_PHOTO)
     dynamodb_repository.update_user_image(user, image_data)
+    return {}, HTTPStatus.NO_CONTENT
+
+
+@api.route("/users/<user_id>/notifications")
+@handle_request(UrlParamsSchema())
+@handle_response(NotificationSchema(many=True))
+def get_user_notifications(url_params, user_id):
+    """Return a list of notification resources."""
+    per_page = url_params.get("per_page", current_app.config["RESULTS_PER_PAGE"])
+    try:
+        results = dynamodb_repository.get_user_notifications(user_id, per_page)
+    except NotFoundException as err:
+        return {"error": str(err)}, HTTPStatus.NOT_FOUND
+    except DatabaseException as err:
+        return {"error": str(str)}, HTTPStatus.BAD_REQUEST
+    return results, HTTPStatus.OK
+
+
+@api.route("/users/<user_id>/notifications/<notification_id>", methods=["PATCH"])
+@handle_request(NotificationSchema())
+@handle_response(None)
+def update_user_notification(notification_data, user_id, notification_id):
+    """Update a user's notification. The two attributes to updated are whether the
+    notification has been read or whether it has been seen.
+    """
+    notification = dynamodb_repository.get_user_notification(user_id, notification_id)
+    if notification.was_seen() and notification_data.get("_seen") is False:
+        return {"error": "Cannot change notification status from seen to unseen"}, HTTPStatus.UNPROCESSABLE_ENTITY
+    if notification.was_read() and notification_data.get("_read") is False:
+        return {"error": "Cannot change notification status from read to unread"}, HTTPStatus.UNPROCESSABLE_ENTITY
+    if notification_data.get("_seen"):
+        notification.mark_as_seen()
+    elif notification_data.get("_read"):
+        notification.mark_as_read()
+    dynamodb_repository.add_user_notification(notification)
     return {}, HTTPStatus.NO_CONTENT
 
