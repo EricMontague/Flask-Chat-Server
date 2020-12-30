05f4252e0afd09c61b75d7cc3a3b82a177fb5339
Eric Montague <eric.g.montague@gmail.com>
2020-12-30 00:19:36 -0500

    Added function to validate whether a file is an image

--- api/app/helpers/files.py
+++ api/app/helpers/files.py
@@ -2,6 +2,7 @@
 
 
 import io
+import imghdr
 from uuid import uuid4
 from PIL import Image
 from werkzeug.utils import secure_filename
@@ -25,6 +26,9 @@ def upload_to_cdn(file, filename):
 
 
 def process_image(user_id, repo, file, image_type):
+    """Upload the given image to S3 and return the
+    data necessary to create an Image model.
+    """
     filename = secure_filename(file.filename)
     image_bytes = io.BytesIO(file.read())
     image_id = user_id + "_" + image_type.name
@@ -42,3 +46,14 @@ def process_image(user_id, repo, file, image_type):
     }
     return image_data
 
+
+def validate_image(file):
+    """Validate that the given file is an image and
+    return its extension if it is.
+    """
+    header = file.read(512)
+    file.seek(0)
+    format = imghdr.what(None, header)
+    if not format:
+        return None
+    return '.' + (format if format != 'jpeg' else 'jpg')
