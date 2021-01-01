a6515b20a9765707076aac67bfcc88e04d3f0632
Eric Montague <eric.g.montague@gmail.com>
2021-01-01 01:16:41 -0500

    Added more routes

--- api/app/api/chats.py
+++ api/app/api/chats.py
@@ -23,13 +23,20 @@ def create_private_chat_message(private_chat_id):
     pass
 
 
-@api.route("/private_chats/<private_chat_id>/messages/message_id", methods=["PATCH"])
+# A message that has been marked as read should not be able to be marked as unread
+@api.route("/private_chats/<private_chat_id>/messages/<message_id>", methods=["PATCH"])
 def update_private_chat_message(private_chat_id, message_id):
     pass
 
 
-@api.route("/private_chats/<private_chat_id>/messages/message_id", methods=["DELETE"])
+@api.route("/private_chats/<private_chat_id>/messages/<message_id>", methods=["DELETE"])
 def delete_private_chat_message(private_chat_id, message_id):
     pass
 
 
+# A user shouldn't have more than one reaction per message
+@api.route(
+    "/private_chats/<private_chat_id>/messages/<message_id>/reactions", methods=["POST"]
+)
+def add_reaction_to_private_chat_message(private_chat_id, message_id):
+    pass
