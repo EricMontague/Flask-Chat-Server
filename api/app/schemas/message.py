30313c3f347e35d21e673a744f0003f501784528
Eric Montague <eric.g.montague@gmail.com>
2021-01-01 01:18:12 -0500

    Created base MessageSchema

--- api/app/schemas/message.py
+++ api/app/schemas/message.py
@@ -4,27 +4,48 @@ deserializing message models.
 
 
 from app.extensions import ma
-
+from marshmallow import validate, EXCLUDE
+from app.schemas.enum_field import EnumField
+from app.models import Reaction
 
 
 class MessageSchema(ma.Schema):
     """Class to serialize and deserialize message models."""
 
-    pass
+    _id = ma.UUID(dump_only=True, data_key="id")
+    _chat_id = ma.UUID(dump_only=True, data_key="chat_id")
+    _content = ma.Str(dump_only=True, data_key="content", validate=validate.Length(min=1, max=500))
+    _created_at = ma.DateTime(dump_only=True, data_key="timestamp")
+    _reactions = ma.List(
+        EnumField(Reaction), 
+        dump_only=True,
+        validate=validate.OneOf([reaction.name for reaction in Reaction])
+    )
+    _read = ma.Boolean(required=True)
+    _editted = ma.Boolean(dump_only=True)
+    resource_type = ma.Str(dump_only=True, default="Message")
 
 
-class PrivateChatMessageSchema(ma.Schema):
+class PrivateChatMessageSchema(MessageSchema):
     """Class to serialize and deserialize message models for 
     private chats.
     """
 
-    pass
+    RESOURCE_NAME = "private_chat_message"
+    COLLECTION_NAME = "private_chat_messages"
+
+    class Meta:
+        unknown = EXCLUDE
 
 
-class GroupChatMessageSchema(ma.Schema):
+class GroupChatMessageSchema(MessageSchema):
     """Class to serialize and deserialize message models for
     group chats.
     """
 
-    pass
+    RESOURCE_NAME = "group_chat_message"
+    COLLECTION_NAME = "group_chat_messages"
+
+    class Meta:
+        unknown = EXCLUDE
 
