721df92dd8b5e6c04039e0d0f2e371bb97055103
Eric Montague <eric.g.montague@gmail.com>
2021-01-05 00:44:03 -0500

    Removed unecessary indexes and fields

--- api/aws_services_setup/global_secondary_indexes.py
+++ api/aws_services_setup/global_secondary_indexes.py
@@ -58,67 +58,12 @@ INVERTED_GSI = {
     },
 }
 
-# GROUP_CHATS_GSI = {
-#     "IndexName": "GroupChatsIndex",
-#     "KeySchema": [
-#         {"AttributeName": "PK", "KeyType": "HASH"},
-#         {"AttributeName": "SK", "KeyType": "RANGE"},
-#     ],
-#     "Projection": {"ProjectionType": "ALL"},
-#     "ProvisionedThroughput": {
-#         "ReadCapacityUnits": os.environ.get("AWS_DYNAMODB_INDEX_RCU", 25),
-#         "WriteCapacityUnits": os.environ.get("AWS_DYNAMODB_INDEX_WCU", 25),
-#     }
-# }
 
-
-# PRIVATE_CHATS_GSI = {
-#     "IndexName": "PrivateChatsIndex",
-#     "KeySchema": [
-#         {"AttributeName": "PK", "KeyType": "HASH"},
-#         {"AttributeName": "SK", "KeyType": "RANGE"},
-#     ],
-#     "Projection": {"ProjectionType": "ALL"},
-#     "ProvisionedThroughput": {
-#         "ReadCapacityUnits": os.environ.get("AWS_DYNAMODB_INDEX_RCU", 25),
-#         "WriteCapacityUnits": os.environ.get("AWS_DYNAMODB_INDEX_WCU", 25),
-#     }
-# }
-
-
-USER_PENDING_REQUESTS_GSI = {
-    "IndexName": "UserPendingRequestsIndex",
-    "KeySchema": [
-        {"AttributeName": "PK", "KeyType": "HASH"},
-        {"AttributeName": "request_status_datetime", "KeyType": "RANGE"},
-    ],
-    "Projection": {"ProjectionType": "ALL"},
-    "ProvisionedThroughput": {
-        "ReadCapacityUnits": os.environ.get("AWS_DYNAMODB_INDEX_RCU", 25),
-        "WriteCapacityUnits": os.environ.get("AWS_DYNAMODB_INDEX_WCU", 25),
-    },
-}
-
-
-GROUP_CHAT_PENDING_REQUESTS_GSI = {
-    "IndexName": "GroupChatPendingRequestsIndex",
-    "KeySchema": [
-        {"AttributeName": "group_chat_id", "KeyType": "HASH"},
-        {"AttributeName": "request_status_datetime", "KeyType": "RANGE"},
-    ],
-    "Projection": {"ProjectionType": "ALL"},
-    "ProvisionedThroughput": {
-        "ReadCapacityUnits": os.environ.get("AWS_DYNAMODB_INDEX_RCU", 25),
-        "WriteCapacityUnits": os.environ.get("AWS_DYNAMODB_INDEX_WCU", 25),
-    },
-}
 
 
 GSI_LIST = [
     USERS_GSI,
     COMMUNITIES_BY_LOCATION_GSI,
     COMMUNITIES_BY_TOPIC_GSI,
-    USER_PENDING_REQUESTS_GSI,
-    GROUP_CHAT_PENDING_REQUESTS_GSI,
     INVERTED_GSI
 ]
--- api/aws_services_setup/utils.py
+++ api/aws_services_setup/utils.py
@@ -11,7 +11,9 @@ from aws_services_setup.global_secondary_indexes import GSI_LIST
 TABLE_NAME = os.environ.get("AWS_DYNAMODB_TABLE_NAME", "ChatAppTable")
 BUCKET_NAME = os.environ.get("AWS_S3_BUCKET_NAME", "chat-app-images")
 BUCKET_LOCATION = os.environ.get("AWS_S3_BUCKET_LOCATION", "us-east-2")
-dynamodb = boto3.resource("dynamodb")
+dynamodb = boto3.resource(
+    "dynamodb", endpoint_url=os.environ.get("AWS_DYNAMODB_ENDPOINT_URL", "http://localhost:8000")
+)
 s3_client = boto3.client("s3")
 
 
@@ -26,8 +28,6 @@ def create_dynamodb_table():
         AttributeDefinitions=[
             {"AttributeName": "PK", "AttributeType": "S"},
             {"AttributeName": "SK", "AttributeType": "S"},
-            {"AttributeName": "request_status_datetime", "AttributeType": "S"},
-            {"AttributeName": "group_chat_id", "AttributeType": "S"},
             {"AttributeName": "USERS_GSI_PK", "AttributeType": "S"},
             {"AttributeName": "USERS_GSI_SK", "AttributeType": "S"},
             {"AttributeName": "COMMUNITIES_BY_TOPIC_GSI_PK", "AttributeType": "S"},
@@ -57,14 +57,11 @@ def create_s3_bucket():
     """Create a bucket in S3."""
     return s3_client.create_bucket(
         Bucket=BUCKET_NAME,
-        CreateBucketConfiguration={
-            "LocationConstraint": BUCKET_LOCATION
-        }
+        CreateBucketConfiguration={"LocationConstraint": BUCKET_LOCATION},
     )
 
 
 def delete_s3_bucket():
     """Delete a bucket from S3."""
-    return s3_client.delete_bucket(
-        Bucket=BUCKET_NAME
-    )
+    return s3_client.delete_bucket(Bucket=BUCKET_NAME)
+
