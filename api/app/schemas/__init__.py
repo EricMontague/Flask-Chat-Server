a16649aca7427ee9b621a1c18dfa932a11b49005
Eric Montague <eric.g.montague@gmail.com>
2021-01-06 00:15:18 -0500

    Added import

--- api/app/schemas/__init__.py
+++ api/app/schemas/__init__.py
@@ -9,6 +9,6 @@ from app.schemas.image import ImageSchema
 from app.schemas.url_parameters import UrlParamsSchema, CommunityUrlParamsSchema
 from app.schemas.community import CommunitySchema
 from app.schemas.notification import NotificationSchema
-from app.schemas.message import PrivateChatMessageSchema
+from app.schemas.message import PrivateChatMessageSchema, GroupChatMessageSchema
 from app.schemas.private_chat import PrivateChatSchema
 from app.schemas.group_chat import GroupChatSchema
