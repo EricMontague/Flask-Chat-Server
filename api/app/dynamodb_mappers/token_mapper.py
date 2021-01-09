2183643d39654e073a38a62b3746d8bfa72c73c1
Eric Montague <eric.g.montague@gmail.com>
2021-01-09 00:36:03 -0500

    Changed the item type and sort key prefix

--- api/app/dynamodb_mappers/token_mapper.py
+++ api/app/dynamodb_mappers/token_mapper.py
@@ -27,8 +27,8 @@ class TokenMapper(ModelMapper):
         partition_key_attribute = "user_id"
         partition_key_prefix = PrimaryKeyPrefix.USER
         sort_key_attribute = "raw_jwt"
-        sort_key_prefix = PrimaryKeyPrefix.ACCESS_TOKEN
-        type_ = ItemType.JWT
+        sort_key_prefix = PrimaryKeyPrefix.JWT_ACCESS_TOKEN
+        type_ = ItemType.JWT_ACCESS_TOKEN
 
     ENUMS = {"token_type": TokenType}
 
