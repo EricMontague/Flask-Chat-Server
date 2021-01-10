59b8a142339e60ad22024366ffc2db4c74e3a672
Eric Montague <eric.g.montague@gmail.com>
2021-01-09 23:41:46 -0500

    Updated imports

--- api/app/helpers/__init__.py
+++ api/app/helpers/__init__.py
@@ -3,5 +3,5 @@ throughout the application.
 """
 
 
-from app.helpers.decorators import handle_request, handle_response, handle_file_request
+from app.helpers.decorators import handle_request, handle_response, handle_file_request, jwt_required
 from app.helpers.files import is_allowed_file_extension, upload_to_cdn, process_image
