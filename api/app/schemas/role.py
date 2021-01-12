b92c651881e2340a0baa83e898e3365593275fb4
Eric Montague <eric.g.montague@gmail.com>
2021-01-12 00:36:24 -0500

    REmoved unecessary method for stripping unwanted fields

--- api/app/schemas/role.py
+++ api/app/schemas/role.py
@@ -6,7 +6,7 @@ and deserializing Role models.
 from app.extensions import ma
 from app.schemas.enum_field import EnumField
 from app.models import RolePermission, RoleName
-from marshmallow import validate, EXCLUDE, pre_load, validates
+from marshmallow import EXCLUDE
 
 
 class RoleSchema(ma.Schema):
@@ -18,11 +18,4 @@ class RoleSchema(ma.Schema):
     name = EnumField(RoleName, dump_only=True)
     _permissions = ma.List(EnumField(RolePermission), required=True, data_key="permissions")
 
-    @pre_load
-    def strip_unwanted_fields(self, data, many, **kwargs):
-        """Remove unwanted fields from the input data before deserialization."""
-        unwanted_fields = ["resource_type"]
-        for field in unwanted_fields:
-            if field in data:
-                data.pop(field)
-        return data
+ 
