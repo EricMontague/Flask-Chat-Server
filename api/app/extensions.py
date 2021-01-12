0c1575d1be703ac662bf4be4269dfe551fb25436
Eric Montague <eric.g.montague@gmail.com>
2021-01-12 00:34:04 -0500

    Reintroduced Flask SocketIO into the application

--- api/app/extensions.py
+++ api/app/extensions.py
@@ -3,9 +3,10 @@
 
 from flask_bcrypt import Bcrypt
 from flask_marshmallow import Marshmallow
+from flask_socketio import SocketIO
 
 
 bcrypt = Bcrypt()
 ma = Marshmallow()
-
+socketio = SocketIO(logger=True, engineio_logger=True)
 
