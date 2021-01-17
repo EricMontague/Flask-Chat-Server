23a3b3e1fdaa53bd46601d92508dfa34d407e3ac
Eric Montague <eric.g.montague@gmail.com>
2021-01-16 20:55:18 -0500

    Created a PrivateChat mapper and renamed the member mappers to be membership mappers'

--- api/app/dynamodb_mappers/chat_mapper.py
+++ api/app/dynamodb_mappers/chat_mapper.py
@@ -6,37 +6,52 @@ items.
 
 from app.dynamodb_mappers.mapper_core import ModelMapper
 from app.dynamodb_mappers.constants import PrimaryKeyPrefix, ItemType
-from app.models import PrivateChatMember, GroupChatMember, GroupChat
+from app.models import PrivateChatMembership, GroupChatMembership, GroupChat, PrivateChat
 
 
-class PrivateChatMemberMapper(ModelMapper):
-    """Class to serialize and deserialize PrivateChatMember models 
+class PrivateChatMapper(ModelMapper):
+    """Class to be used only to serialize PrivateChat models
+    to DynamoDB items.
+    """
+
+    class Meta:
+        model = PrivateChat
+        fields = ("_id", "primary_user_id", "secondary_user_id")
+        partition_key_attribute = "_id"
+        partition_key_prefix = PrimaryKeyPrefix.PRIVATE_CHAT
+        sort_key_attribute = "_id"
+        sory_key_prefix = PrimaryKeyPrefix.PRIVATE_CHAT
+        type_ = ItemType.PRIVATE_CHAT.name
+
+
+class PrivateChatMembershipMapper(ModelMapper):
+    """Class to serialize and deserialize PrivateChatMembership models 
     to and from DynamoDB items.
     """
 
     class Meta:
-        model = PrivateChatMember
+        model = PrivateChatMembership
         fields = ("private_chat_id", "user_id", "other_user_id", "created_at")
         partition_key_attribute = "user_id"
         partition_key_prefix = PrimaryKeyPrefix.USER
         sort_key_attribute = "private_chat_id"
         sort_key_prefix = PrimaryKeyPrefix.PRIVATE_CHAT
-        type_ = ItemType.PRIVATE_CHAT_MEMBER.name
+        type_ = ItemType.PRIVATE_CHAT_MEMBERSHIP.name
 
 
-class GroupChatMemberMapper(ModelMapper):
-    """Class to serialize and deserialize GroupChatMember models 
+class GroupChatMembershipMapper(ModelMapper):
+    """Class to serialize and deserialize GroupChatMembership models 
     to and from DynamoDB items.
     """
 
     class Meta:
-        model = GroupChatMember
+        model = GroupChatMembership
         fields = ("group_chat_id", "community_id", "user_id", "created_at")
         partition_key_attribute = "group_chat_id"
         partition_key_prefix = PrimaryKeyPrefix.GROUP_CHAT
         sort_key_attribute = "user_id"
         sort_key_prefix = PrimaryKeyPrefix.USER
-        type_ = ItemType.GROUP_CHAT_MEMBER.name
+        type_ = ItemType.GROUP_CHAT_MEMBERSHIP.name
 
 
 class GroupChatMapper(ModelMapper):
