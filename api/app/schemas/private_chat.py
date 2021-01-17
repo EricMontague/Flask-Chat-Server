5afbb529627032097187663549557754c9eee939
Eric Montague <eric.g.montague@gmail.com>
2021-01-16 20:57:04 -0500

    Added a data_key for the private chat id

--- api/app/schemas/private_chat.py
+++ api/app/schemas/private_chat.py
@@ -13,12 +13,14 @@ class PrivateChatSchema(ma.Schema):
     RESOURCE_NAME = "private_chat"
     COLLECTION_NAME = "private_chats"
 
-    _id = ma.UUID(dump_only=True)
+    _id = ma.UUID(dump_only=True, data_key="id")
     _primary_user = ma.Nested(UserSchema(), dump_only=True, data_key="primary_user")
     _secondary_user = ma.Nested(UserSchema(), dump_only=True, data_key="secondary_user")
     resource_type = ma.Str(dump_only=True, default="PrivateChat")
 
     # Links
     messages_url = ma.URLFor("api.get_private_chat_messages", private_chat_id="<_id>")
+
+    
     
     
