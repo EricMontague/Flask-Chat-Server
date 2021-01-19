beefb974f5985bcf6caf4f140b2b9893f0e676ae
Eric Montague <eric.g.montague@gmail.com>
2021-01-19 00:43:54 -0500

    Manually tested event handlers and fixed bugs invloving schemas

--- api/app/sockets/messages.py
+++ api/app/sockets/messages.py
@@ -7,15 +7,15 @@ from flask_socketio import emit
 from app.extensions import socketio
 from app.decorators.auth import socketio_jwt_required, socketio_permission_required
 from app.decorators.views import socketio_handle_arguments
-from app.models import TokenType, RolePermission, Reaction, ReactionType
-from app.schemas import GroupChatMessageSchema, ReactionSchema
+from app.models import TokenType, RolePermission, Reaction, MessageType
+from app.schemas import GroupChatMessageSchema, PrivateChatMessageSchema, MessageSchema, ReactionSchema
 from app.repositories import dynamodb_repository
 
 
 @socketio.event
 @socketio_jwt_required(TokenType.ACCESS_TOKEN)
 @socketio_permission_required(RolePermission.WRITE_CHAT_MESSAGE)
-@socketio_handle_arguments(GroupChatMessageSchema())
+@socketio_handle_arguments(MessageSchema())
 def update_chat_message(message_data, message_schema):
     """Update the content of a chat message."""
     chat_message = dynamodb_repository.get_chat_message(
@@ -28,9 +28,13 @@ def update_chat_message(message_data, message_schema):
     else:
         chat_message.edit(message_data["_content"])
         dynamodb_repository.add_chat_message(chat_message)
+        if chat_message.message_type == MessageType.PRIVATE_CHAT:
+            schema = PrivateChatMessageSchema()
+        else:
+            schema = GroupChatMessageSchema()
         emit(
             "chat_message_editted",
-            message_schema.dumps(chat_message),
+            schema.dumps(chat_message),
             room=chat_message.chat_id
         )
 
@@ -38,7 +42,7 @@ def update_chat_message(message_data, message_schema):
 @socketio.event
 @socketio_jwt_required(TokenType.ACCESS_TOKEN)
 @socketio_permission_required(RolePermission.WRITE_CHAT_MESSAGE)
-@socketio_handle_arguments(GroupChatMessageSchema(only=["_id", "_chat_id"]))
+@socketio_handle_arguments(MessageSchema(only=["_id", "_chat_id", "message_type"]))
 def delete_chat_message(message_data, message_schema):
     """Delete an existing chat message."""
     chat_message = dynamodb_repository.get_chat_message(
@@ -52,7 +56,7 @@ def delete_chat_message(message_data, message_schema):
         dynamodb_repository.remove_chat_message(chat_message)
         emit(
             "chat_message_deleted",
-            json.dumps({"message_id": chat_message.id, "message_type": message_data["message_type"]}),
+            json.dumps({"message_id": chat_message.id, "message_type": message_data["message_type"].name}),
             room=chat_message.chat_id
         )
 
@@ -71,15 +75,12 @@ def react_to_chat_message(reaction_data, reaction_schema):
     elif chat_message.user_id != g.current_user.id:
         emit("error", json.dumps({"error": "User is not the sender of this message"}))
     else:
-        reaction = Reaction(g.current_user.id, ReactionType[reaction_data["reaction_type"].upper()])
+        reaction = Reaction(g.current_user.id, reaction_data["reaction_type"], reaction_data["message_id"])
         chat_message.add_reaction(reaction)
         dynamodb_repository.add_chat_message(chat_message)
         emit(
             "new_chat_message_reaction",
-            json.dumps({
-                "reaction": reaction_schema.dumps(reaction),
-                "message_id": chat_message.id
-            }),
+            reaction_schema.dumps(reaction),
             room=chat_message.chat_id,
         )
 
@@ -87,7 +88,7 @@ def react_to_chat_message(reaction_data, reaction_schema):
 @socketio.event
 @socketio_jwt_required(TokenType.ACCESS_TOKEN)
 @socketio_permission_required(RolePermission.WRITE_CHAT_MESSAGE)
-@socketio_handle_arguments(ReactionSchema(only=["chat_id", "message_id"]))
+@socketio_handle_arguments(ReactionSchema(partial=["reaction_type"]))
 def unreact_to_chat_message(reaction_data, reaction_schema):
     """Remove a reaction from a chat message."""
     chat_message = dynamodb_repository.get_chat_message(
@@ -105,10 +106,7 @@ def unreact_to_chat_message(reaction_data, reaction_schema):
             dynamodb_repository.add_chat_message(chat_message)
             emit(
                 "removed_chat_message_reaction",
-                json.dumps({
-                    "reaction": reaction_schema.dumps(reaction),
-                    "message_id": chat_message.id
-                }),
+                reaction_schema.dumps(reaction),
                 room=chat_message.chat_id,
             )
 
