ab463aa375cd9710c3c2366f19e1ade902d9ba0e
Eric Montague <eric.g.montague@gmail.com>
2021-01-19 00:46:04 -0500

    Made id a required field and added a convert UUID to hex method

--- api/app/schemas/private_chat.py
+++ api/app/schemas/private_chat.py
@@ -3,8 +3,10 @@ serialize PrivateChat models.
 """
 
 
+import uuid
 from app.extensions import ma
 from app.schemas.user import UserSchema
+from marshmallow import post_load
 
 
 class PrivateChatSchema(ma.Schema):
@@ -13,7 +15,7 @@ class PrivateChatSchema(ma.Schema):
     RESOURCE_NAME = "private_chat"
     COLLECTION_NAME = "private_chats"
 
-    _id = ma.UUID(dump_only=True, data_key="id")
+    _id = ma.UUID(required=True, data_key="id")
     _primary_user = ma.Nested(UserSchema(), dump_only=True, data_key="primary_user")
     _secondary_user = ma.Nested(UserSchema(), dump_only=True, data_key="secondary_user")
     resource_type = ma.Str(dump_only=True, default="PrivateChat")
@@ -21,6 +23,15 @@ class PrivateChatSchema(ma.Schema):
     # Links
     messages_url = ma.URLFor("api.get_private_chat_messages", private_chat_id="<_id>")
 
+
+    @post_load
+    def convert_uuid_to_hex(self, data, **kwargs):
+        """Convert all UUID fields to their 32-character hexadecimal equivalent."""
+        for key in data:
+            value = data[key]
+            if isinstance(value, uuid.UUID):
+                data[key] = data[key].hex
+        return data
     
     
     
