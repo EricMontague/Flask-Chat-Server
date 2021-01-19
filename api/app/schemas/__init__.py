793db917aa5561c7560069e207a3f44b452f6d44
Eric Montague <eric.g.montague@gmail.com>
2021-01-19 00:46:32 -0500

    Updated imports

--- api/app/schemas/__init__.py
+++ api/app/schemas/__init__.py
@@ -9,7 +9,7 @@ from app.schemas.image import ImageSchema
 from app.schemas.url_parameters import UrlParamsSchema, CommunityUrlParamsSchema
 from app.schemas.community import CommunitySchema
 from app.schemas.notification import NotificationSchema
-from app.schemas.message import PrivateChatMessageSchema, GroupChatMessageSchema, ReactionSchema
+from app.schemas.message import PrivateChatMessageSchema, GroupChatMessageSchema, MessageSchema, ReactionSchema
 from app.schemas.private_chat import PrivateChatSchema
 from app.schemas.group_chat import GroupChatSchema
 from app.schemas.role import RoleSchema
