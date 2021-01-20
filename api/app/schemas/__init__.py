071d126fc821e70652b6df9c5481fe4e86c6514b
Eric Montague <eric.g.montague@gmail.com>
2021-01-20 01:03:53 -0500

    Updated imports

--- api/app/schemas/__init__.py
+++ api/app/schemas/__init__.py
@@ -6,7 +6,7 @@ deserializing models.
 from app.schemas.user import UserSchema
 from app.schemas.location import LocationSchema
 from app.schemas.image import ImageSchema
-from app.schemas.url_parameters import UrlParamsSchema, CommunityUrlParamsSchema
+from app.schemas.url_parameters import UrlParamsSchema, CommunityUrlParamsSchema, GroupChatUrlParamsSchema
 from app.schemas.community import CommunitySchema
 from app.schemas.notification import NotificationSchema
 from app.schemas.message import PrivateChatMessageSchema, GroupChatMessageSchema, MessageSchema, ReactionSchema
