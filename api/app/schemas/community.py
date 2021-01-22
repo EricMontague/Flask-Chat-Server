fbc7aef2cad16ddf996d7f521413ad8ddae116a2
Eric Montague <eric.g.montague@gmail.com>
2021-01-22 01:29:47 -0500

    Added founder id to community schema

--- api/app/schemas/community.py
+++ api/app/schemas/community.py
@@ -27,6 +27,7 @@ class CommunitySchema(ma.Schema):
     avatar = ma.Nested(ImageSchema, dump_only=True)
     cover_photo = ma.Nested(ImageSchema, dump_only=True)
     location = ma.Nested(LocationSchema, required=True)
+    _founder_id = ma.UUID(dump_only=True, data_key="founder_id")
     _created_at = ma.DateTime(
         data_key="founded_on", dump_only=True
     )  # defaults to ISO 8601
@@ -34,6 +35,7 @@ class CommunitySchema(ma.Schema):
 
     # Links
     self_url = ma.URLFor("api.get_community", community_id="<_id>")
+    founder_url = ma.URLFor("api.get_user", user_id="<_founder_id>")
     members_url = ma.URLFor("api.get_community_members", community_id="<_id>")
     group_chats_url = ma.URLFor("api.get_community_group_chats", community_id="<_id>")
 
