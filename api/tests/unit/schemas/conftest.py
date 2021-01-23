1e5c759bedba241cc40c8496cb8f9f09c67247c3
Eric Montague <eric.g.montague@gmail.com>
2021-01-22 21:31:09 -0500

    Created flask_app fixture

--- api/tests/unit/schemas/conftest.py
+++ api/tests/unit/schemas/conftest.py
@@ -5,8 +5,23 @@ on marshmallow schemas.
 
 import pytest
 from app.schemas import UserSchema, LocationSchema, ImageSchema, UrlParamsSchema
-from app.models.user_factory import UserFactory
+from app.models.factories import UserFactory
+from app import create_app
 from test_data import partial_user_data, complete_user_data
+
+
+@pytest.fixture
+def flask_app():
+    """Create an instance of the application and push an application
+    context.
+    """
+    app = create_app(config_name="testing")
+    app.config["SERVER_NAME"] = "http://localhost:5000"
+    print(app.config["TESTING"])
+    context = app.app_context()
+    context.push()
+    yield app
+    context.pop()
 
 
 @pytest.fixture
