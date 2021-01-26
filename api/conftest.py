e20115e5b5939f9dae7c061943f67fd949574716
Eric Montague <eric.g.montague@gmail.com>
2021-01-25 22:18:08 -0500

    Import dotenv to load environment variables for tests

--- api/conftest.py
+++ api/conftest.py
@@ -1,4 +1,11 @@
-"""This file is kept empty and it meant as a workaround for
+"""This file is meant as a workaround for
 adding this directory to the PYTHONPATH when running tests
-with pytest.
+with pytest. It is also used for making enviroment variables
+available before tests
 """
+
+
+from dotenv import load_dotenv
+
+
+load_dotenv()
