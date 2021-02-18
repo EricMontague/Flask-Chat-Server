53e9402fed6f9fe9e7faef956229038038da0823
Eric Montague <eric.g.montague@gmail.com>
2021-02-17 19:14:32 -0500

    Created initial skeleton for App component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,36 @@
 import React from "react";
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route
+} from "react-router-dom";
+import { ThemeProvider } from "styled-components";
+import theme from "./theme";
+import SignInPage from "./SignInPage";
+import SignUpPage from "./SignUpPage";
+import GlobalStyle from "./globalStyle";
+
 
 
 const App = () => {
-    return <h1>Hello, World</h1>;
+    return (
+        <Router>
+            <ThemeProvider theme= { theme }>
+                <GlobalStyle />
+                <Switch>
+                    <Route exact path="/signin" >
+                        <SignInPage />
+                    </Route>
+                    <Route exact path="/signup">
+                        <SignUpPage />
+                    </Route>
+                    <Route exact path="/">
+                        <h1>Hello, World!</h1>
+                    </Route>
+                </Switch>
+            </ThemeProvider>
+        </Router>
+    )
 };
 
 
