1425e8cbc8c600b151d5ea2715f918c0a902bf99
Eric Montague <eric.g.montague@gmail.com>
2021-02-17 19:14:15 -0500

    Created global style component

---
+++ src/globalStyle.js
@@ -0,0 +1,46 @@
+import { createGlobalStyle } from "styled-components";
+import theme from "./theme"
+
+
+const GlobalStyle = createGlobalStyle`
+
+    * {
+        box-sizing: border-box;
+        margin: 0;
+        padding: 0;
+        -webkit-font-smoothing: auto;
+        font-weight: inherit;
+        -webkit-appearance: none;
+        -moz-appearance: none;
+    }
+
+    html {
+        font-size: 16px;
+        
+    }
+
+    body {
+        -webkit-overflow-scrolling: touch;
+        overscroll-behavior-y: none;
+        color: ${theme.text.default}
+        line-height: 1.5;
+        font-family: ${theme.font.default};
+        background-color: ${theme.bg.wash};
+    }
+
+    a {
+        text-decoration: none;
+    }
+
+    a:hover {
+        cursor: pointer;
+    }
+
+    ul {
+        list-style: none;
+    }
+
+`;
+
+
+export default GlobalStyle;
