3551f059fc53b3be1719e9c011a621b64842d70d
Eric Montague <eric.g.montague@gmail.com>
2021-02-22 20:11:53 -0500

    Created new StyledForm component

---
+++ src/components/forms/styles.js
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+
+
+export const StyledForm = styled.form`
+    display: flex;
+    flex-direction: column;
+    align-self: stretch;
+    flex: none;
+    max-width: 100%;
+
+    
+`;
