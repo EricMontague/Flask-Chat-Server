1f51468bbc6f6fc4a2670c2069456708f32a7049
Eric Montague <eric.g.montague@gmail.com>
2021-02-22 20:09:27 -0500

    Created StyledInputContainer component and expanded on the StyledInput

--- src/components/formElements/styles.js
+++ src/components/formElements/styles.js
@@ -1,26 +1,54 @@
 import styled from "styled-components";
-import {zIndex} from "../globals";
+import {zIndex, FlexRow} from "../globals";
 
 
 export const StyledLabel = styled.label`
     color: ${props => props.theme.text.default};
-    margin-top: 0.75rem;
+    margin-bottom: 0.75rem;
     z-index: ${zIndex.formElement};
 `;
 
 
 export const StyledInput = styled.input`
-    border: 1px solid #edeff1;
+    flex: 1 0 auto;
+    border: none;
     border-radius: 0.25rem;
-    background: ${props => props.theme.bg.default};
-    padding: 0.5rem 1rem;
-    margin-top: 0.5rem;
-    margin-bottom: 0.5rem;
-    transition: border-color 0.3s ease-in-out;
-    z-index: ${zIndex.formElement};
+    background: none;
+    font-size: 1rem;
+    padding: 1.25rem;
+    width: 100%;
+    color ${props => props.theme.text.default};
+    
 
     &:focus {
-        border-color: #52a8ec;
+        outline: none;
     }
+
+    &::placeholder{
+        color: ${props => props.theme.input.placeholder};
+        letter-spacing: 0.03rem;
+    }
+`;
+
+
+export const StyledInputContainer = styled(FlexRow)`
+    flex: 1 1 auto;
+    position: relative;
+    margin-bottom: 1rem;
+    border-radius: 0.25rem;
+    border: 1px solid ${props => props.theme.input.border};
+    background: ${props => props.theme.bg.default};
+    transition: all 0.25s ease;
+    z-index: ${zIndex.formElement};
+
+    &:hover {
+        border-color: #e0e3e5;
+    }
+
+    &:focus-within {
+        border-color: ${props => props.theme.bg.primary};
+    }
+
+    
 `;
 
