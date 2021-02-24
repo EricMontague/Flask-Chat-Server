78e6e226d0c70bba3fbf85f0110a95c7a4ea6db7
Eric Montague <eric.g.montague@gmail.com>
2021-02-23 19:29:47 -0500

    Changed FinePrint component to SignInReminderand starting working on the layout of the navbar

--- src/pages/SignUpPage.js
+++ src/pages/SignUpPage.js
@@ -1,26 +1,50 @@
 import React from "react";
 import Helmet from "react-helmet";
-import {StyledLink} from "../components/buttons/styles";
+import {StyledLink, StyledHoverLink} from "../components/buttons/styles";
+import {OutlineButton} from "../components/buttons";
 import styled, {withTheme} from "styled-components";
 import PropTypes from "prop-types";
 import {StyledNavbarTransparent, StyledNavbarMenu, StyledNavbarMenuItem} from "../components/navigation";
 import {SignUpForm} from "../components/forms";
-import {P, H2, FlexRow, FlexCol} from "../components/globals";
+import {fontStack, flexCenteredMixin, FlexRow, FlexCol} from "../components/globals";
 
 
 const Logo = styled(StyledLink)`
-    color: ${props => props.color ? props.color : props.theme.text.default};
+    ${fontStack};
+    flex: 1 0 auto;
+    letter-spacing: 0.03rem;
+    color: ${props => props.theme.text.white};
     font-weight: 600;
+    font-size: 1.5rem;
 `;
 
 
-const FinePrint = styled(P)`
-    display: flex;
-    align-items: center;
-    justify-content: center;
+const SignInReminder = styled.div`
+    ${fontStack};
+    ${flexCenteredMixin};
     padding: 0.5rem;
-    margin: 1rem 0;
-    color: ${props => props.theme.text.default};
+    margin: 1.5rem 0;
+    color: ${props => props.theme.text.white};
+
+    & > p {
+        margin-right: 0.3rem;
+        font-size: 0.95rem;
+    }
+
+    & > a {
+        font-size: 0.95rem;
+    }
+`;
+
+
+
+const PageTitle = styled.h1`
+    ${fontStack};
+    font-weight: 300;
+    font-size: 1.75rem;
+    margin-bottom: 2rem;
+    color: ${props => props.theme.text.white};
+    letter-spacing: 0.03rem;
 `;
 
 
@@ -38,25 +62,27 @@ const SignUpPage = props => {
             <StyledNavbarTransparent>
                 <FlexRow>
                     <Logo to={"/"}>TalkChat</Logo>
-                    <StyledNavbarMenu>
+                    <StyledNavbarMenu justifyContent="flex-end">
                         <StyledNavbarMenuItem>
-                            <StyledLink to="/sign-in">Sign In</StyledLink>
+                            <StyledHoverLink to="/sign-in" color={props.theme.text.white}>
+                                Sign In
+                            </StyledHoverLink>
                         </StyledNavbarMenuItem>
                         <StyledNavbarMenuItem>
-                            <StyledLink to="/sign-up">Sign Up</StyledLink>
+                            <OutlineButton to="/sign-up">Sign Up</OutlineButton>
                         </StyledNavbarMenuItem>
                     </StyledNavbarMenu>
                 </FlexRow>
             </StyledNavbarTransparent>
             <FlexCol justifyContent="center" alignItems="center">
-                <H2>Create your account</H2>    
+                <PageTitle>Create your account</PageTitle>    
                 <SignUpForm />
-                <FinePrint>
-                    Already have an account? 
-                    <StyledLink to="/sign-in">
+                <SignInReminder>
+                    <p>Already have an account?</p>
+                    <StyledHoverLink to="/sign-in" color={props.theme.text.white}>
                         Sign In
-                    </StyledLink>
-                </FinePrint>
+                    </StyledHoverLink>
+                </SignInReminder>
             </FlexCol>
         </>
     )
