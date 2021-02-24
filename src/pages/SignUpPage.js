4f08e744ae85398476919b34df07f1399ecf6f13
Eric Montague <eric.g.montague@gmail.com>
2021-02-24 17:14:56 -0500

    Moved the Navbar into a file of its own and made turned it into its own component

--- src/pages/SignUpPage.js
+++ src/pages/SignUpPage.js
@@ -1,22 +1,12 @@
 import React from "react";
-import Helmet from "react-helmet";
-import {StyledLink, StyledHoverLink} from "../components/buttons/styles";
-import {OutlineButton} from "../components/buttons";
-import styled, {withTheme} from "styled-components";
 import PropTypes from "prop-types";
-import {StyledNavbarTransparent, StyledNavbarMenu, StyledNavbarMenuItem} from "../components/navigation";
+import Helmet from "react-helmet";
+import styled, {withTheme} from "styled-components";
+import {StyledHoverLink} from "../components/buttons/styles";
+import {TopNavbarTransparent} from "../components/navigation";
 import {SignUpForm} from "../components/forms";
-import {fontStack, flexCenteredMixin, FlexRow, FlexCol} from "../components/globals";
+import {fontStack, flexCenteredMixin, FlexCol} from "../components/globals";
 
-
-const Logo = styled(StyledLink)`
-    ${fontStack};
-    flex: 1 0 auto;
-    letter-spacing: 0.03rem;
-    color: ${props => props.theme.text.white};
-    font-weight: 600;
-    font-size: 1.5rem;
-`;
 
 
 const SignInReminder = styled.div`
@@ -42,7 +32,7 @@ const PageTitle = styled.h1`
     ${fontStack};
     font-weight: 300;
     font-size: 1.75rem;
-    margin-bottom: 2rem;
+    margin: 2rem 0;
     color: ${props => props.theme.text.white};
     letter-spacing: 0.03rem;
 `;
@@ -52,28 +42,14 @@ const SignUpPage = props => {
     return (
         <>
             <Helmet>
-                <title>App Name - Sign Up</title>
+                <title>ChatterBox - Sign Up</title>
                 <style type="text/css">{`
                     body {
                         background-color: ${props.theme.bg.primary};
                     }
                 `}</style>
             </Helmet>
-            <StyledNavbarTransparent>
-                <FlexRow>
-                    <Logo to={"/"}>TalkChat</Logo>
-                    <StyledNavbarMenu justifyContent="flex-end">
-                        <StyledNavbarMenuItem>
-                            <StyledHoverLink to="/sign-in" color={props.theme.text.white}>
-                                Sign In
-                            </StyledHoverLink>
-                        </StyledNavbarMenuItem>
-                        <StyledNavbarMenuItem>
-                            <OutlineButton to="/sign-up">Sign Up</OutlineButton>
-                        </StyledNavbarMenuItem>
-                    </StyledNavbarMenu>
-                </FlexRow>
-            </StyledNavbarTransparent>
+            <TopNavbarTransparent linkColor={props.theme.text.white}/>
             <FlexCol justifyContent="center" alignItems="center">
                 <PageTitle>Create your account</PageTitle>    
                 <SignUpForm />
