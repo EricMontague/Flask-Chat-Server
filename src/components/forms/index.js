ad6d98bf558a0ac0851a3a0440e4b49b4711d2a5
Eric Montague <eric.g.montague@gmail.com>
2021-02-25 16:31:08 -0500

    Started working to integrate Formik

--- src/components/forms/index.js
+++ src/components/forms/index.js
@@ -1,4 +1,5 @@
 import React from "react";
+import {useFormik} from "formik";
 import {StyledCard, StyledCardBody} from "../cards/styles";
 import {StyledForm} from "./styles";
 import {Input} from "../formElements";
@@ -6,85 +7,93 @@ import {StyledInputRow} from "../formElements/styles";
 import {PrimaryButton} from "../buttons";
 
 
-export class SignUpForm extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            username: "",
-            name: "",
-            email: "",
-            password: "",
-            isFormCompleted: false
-        };
-        this.handleChange = this.handleChange.bind(this);
-        this.handleSubmit = this.handleSubmit.bind(this);
+export const SignUpForm = props => {
+
+    const isFormCompleted = values => {
+        for (const field in values) {
+            if (values[field] === "") {
+                return false;
+            }
+        }
+        return true;
     }
 
-    handleChange(event) {
-        this.setState({[event.target.name]: event.target.value});
-    }
-
-    handleSubmit(event) {
-        event.preventDefault();
-        alert("Login form submitted!");
-    }
-
-    render() {
-        return (
-            <StyledCard maxWidth="30rem">
-                <StyledCardBody>
-                    <StyledForm onSubmit={this.handleSubmit}>
-                        <StyledInputRow>
-                            <Input
-                                id="username" 
-                                type="text" 
-                                placeholder="Username"
-                                name="username"
-                                value={this.state.username}
-                                withLabel={false}
-                                handleChange={this.handleChange}
-                            />
-                            <Input
-                                id="name"
-                                type="text" 
-                                placeholder="Name"
-                                name="name"
-                                value={this.state.name}
-                                withLabel={false}
-                                handleChange={this.handleChange}
-                            />
-                        </StyledInputRow>
-                        <StyledInputRow>
-                            <Input
-                                id="email"
-                                type="text" 
-                                placeholder="Email"
-                                name="email"
-                                value={this.state.email}
-                                withLabel={false}
-                                handleChange={this.handleChange}
-                            />
-                        </StyledInputRow>
-                        <StyledInputRow>
-                            <Input
-                                id="password"
-                                type="password" 
-                                placeholder="Choose password"
-                                name="password"
-                                value={this.state.password}
-                                withLabel={false}
-                                handleChange={this.handleChange}
-                            />
-                        </StyledInputRow>
-                        {/* <LocationField /> */}
-                        <PrimaryButton disabled={!this.state.isFormCompleted} width="100%" padding="1.5rem" type="submit">
-                            Create account
-                        </PrimaryButton>
-                    </StyledForm>
-                </StyledCardBody>
-            </StyledCard>
-        )
-    }
+    
+    return (
+        <StyledCard maxWidth="30rem">
+            <StyledCardBody>
+                <Formik
+                    initialValues={{
+                        username: "",
+                        name: "",
+                        email: "",
+                        password: ""
+                    }}
+                    onSubmit={(values, {setSubmitting}) => {
+                        console.log(values);
+                        setSubmitting(false);
+                    }}
+                >
+                    {({
+                        values,
+                        handleChange,
+                        handleSubmit,
+                        isSubmitting
+                    }) => (
+                        <StyledForm onSubmit={handleSubmit}>
+                            <StyledInputRow>
+                                <Input
+                                    id="username" 
+                                    type="text" 
+                                    placeholder="Username"
+                                    name="username"
+                                    value={values.username}
+                                    withLabel={false}
+                                    handleChange={handleChange}
+                                />
+                                <Input
+                                    id="name"
+                                    type="text" 
+                                    placeholder="Name"
+                                    name="name"
+                                    value={values.name}
+                                    withLabel={false}
+                                    handleChange={handleChange}
+                                />
+                            </StyledInputRow>
+                            <StyledInputRow>
+                                <Input
+                                    id="email"
+                                    type="text" 
+                                    placeholder="Email"
+                                    name="email"
+                                    value={values.email}
+                                    withLabel={false}
+                                    handleChange={handleChange}
+                                />
+                            </StyledInputRow>
+                            <StyledInputRow>
+                                <Input
+                                    id="password"
+                                    type="password" 
+                                    placeholder="Choose password"
+                                    name="password"
+                                    value={values.password}
+                                    withLabel={false}
+                                    handleChange={handleChange}
+                                />
+                            </StyledInputRow>
+                            {/* <LocationField /> */}
+                            <PrimaryButton disabled={!isFormCompleted(values)} width="100%" padding="1.5rem" type="submit">
+                                Create account
+                            </PrimaryButton>
+                        </StyledForm>
+                    )}
+                </Formik>
+            </StyledCardBody>
+        </StyledCard>
+    );
+    
     
 };
 
