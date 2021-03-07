81470ae2bc763aa72a51dcb135c7dec61b4e7576
Eric Montague <eric.g.montague@gmail.com>
2021-03-07 17:51:37 -0500

    Refactored styles so that the styling of the input is done on the input itself rater than the input container

--- src/components/formElements/styles.js
+++ src/components/formElements/styles.js
@@ -3,53 +3,49 @@ import {zIndex, FlexRow, Span} from "../globals";
 
 
 export const StyledLabel = styled.label`
-    color: ${props => props.theme.text.default};
-    margin-bottom: 0.75rem;
+    display: inline-block;
+    color: #333;
+    padding-bottom: 0.75rem;
     z-index: ${zIndex.formElement};
 `;
 
 
 export const StyledInput = styled.input`
-    flex: 1 0 auto;
-    border: none;
+    flex: 1 1 auto;
+    width: 100%;
+    border: 1px solid ${props => props.theme.input.border};
     border-radius: 0.25rem;
-    background: none;
+    background: ${props => props.theme.bg.default};
     font-size: 1rem;
     padding: 1.25rem;
-    width: 100%;
     color ${props => props.theme.text.default};
+    transition: all 0.25s ease;
     
+
+    &:hover {
+        border-color: #e0e3e5;
+    }
 
     &:focus {
         outline: none;
+        border-color: ${props => props.theme.bg.primary};
     }
 
     &::placeholder{
         color: ${props => props.theme.input.placeholder};
         letter-spacing: 0.03rem;
     }
+
+    
 `;
 
 
-export const StyledInputContainer = styled(FlexRow)`
-    flex: 1 1 auto;
+export const StyledInputContainer = styled.div`
+    flex: 1 1 auto%;
+    width: 100%;
     position: relative;
     margin-bottom: 1rem;
-    border-radius: 0.25rem;
-    border: 1px solid ${props => props.theme.input.border};
-    background: ${props => props.theme.bg.default};
-    transition: all 0.25s ease;
-    z-index: ${zIndex.formElement};
-
-    &:hover {
-        border-color: #e0e3e5;
-    }
-
-    &:focus-within {
-        border-color: ${props => props.theme.bg.primary};
-    }
-
-    
+    z-index: ${zIndex.formElement};    
 `;
 
 
@@ -62,9 +58,8 @@ export const StyledInputRow = styled(FlexRow)`
 
 
 export const StyledErrorMessage = styled(Span)`
-    flex: 1 0 100%;
+    flex: 1 1 100%;
     color: ${props => props.theme.text.warn};
-    margin-top: -0.25rem;
-    padding-bottom: 0.75rem;
+    margin-bottom: 1rem;
 `;
 
