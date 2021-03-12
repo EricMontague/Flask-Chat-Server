d464130822b09c33d9beac6c767c44f3ba57a72e
Eric Montague <eric.g.montague@gmail.com>
2021-03-12 12:22:01 -0500

    Removed unecessary React import

--- src/components/navigation/index.js
+++ src/components/navigation/index.js
@@ -1,4 +1,3 @@
-import React from "react";
 import PropTypes from "prop-types";
 import {FlexRow} from "../globals";
 import {StyledNavbarTransparent, StyledNavbarMenu, StyledNavbarMenuItem} from "./styles";
