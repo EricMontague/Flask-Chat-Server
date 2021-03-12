413b4ebf685a4197c66f1dcfd68f70606205b900
Eric Montague <eric.g.montague@gmail.com>
2021-03-12 15:49:36 -0500

    Updated imports for SignInPage and changed quotes to be single quotes

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
-import React from "react";
+import React from 'react';
 import {
     BrowserRouter as Router,
     Switch,
     Route
-} from "react-router-dom";
-import { ThemeProvider } from "styled-components";
-import theme from "./theme";
-import SignInPage from "./pages/SignInPage";
-import SignUpPage from "./pages/SignUpPage";
-import GlobalStyle from "./globalStyle";
+} from 'react-router-dom';
+import { ThemeProvider } from 'styled-components';
+import theme from './theme';
+import SignInPage from './views/signin/SignInPage';
+import SignUpPage from './views/signup/SignUpPage';
+import GlobalStyle from './globalStyle';
 
 
 
@@ -18,14 +18,17 @@ const App = () => {
             <ThemeProvider theme= { theme }>
                 <GlobalStyle />
                 <Switch>
-                    <Route exact path="/sign-in" >
+                    <Route exact path='/sign-in' >
                         <SignInPage />
                     </Route>
-                    <Route exact path="/sign-up">
+                    <Route exact path='/sign-up'>
                         <SignUpPage />
                     </Route>
-                    <Route exact path="/">
+                    <Route exact path='/'>
                         <h1>Hello, World!</h1>
+                    </Route>
+                    <Route path='*'>
+                        <h1>Page does not Exist!</h1>
                     </Route>
                 </Switch>
             </ThemeProvider>
