57ee80071efb777c216ddda4c6d5a345f88a4918
Eric Montague <eric.g.montague@gmail.com>
2021-04-10 19:36:51 -0400

    Made the store exportable

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,7 +1,7 @@
 import { configureStore } from '@reduxjs/toolkit';
 import usersReducer  from '../features/users/usersSlice';
 
-const store = configureStore({
+export const store = configureStore({
     reducer: {
         users: usersReducer,
     }
