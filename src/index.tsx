2d68bc4482bfbe6e8bc31e625adff06e3607d808
Eric Montague <eric.g.montague@gmail.com>
2021-04-13 19:32:10 -0400

    Moved up the import of the store

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { store } from './app/store';
 import App from './app/App';
 import { Provider } from 'react-redux';
-import { store } from './app/store';
 
 ReactDOM.render(
     <Provider store={store}>
