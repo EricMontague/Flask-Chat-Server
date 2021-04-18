d23c76e205f0bf5e462ad68390ab6f9f3ac2eb7b
Eric Montague <eric.g.montague@gmail.com>
2021-04-18 19:09:28 -0400

    Added new constants

--- src/constants.ts
+++ src/constants.ts
@@ -1,3 +1,7 @@
 export const GOOGLE_PLACES_API_FIELDS = ['place_id', 'name'];
 export const GOOGLE_PLACES_API_TYPES = ['(cities)'];
 export const GOOGLE_PLACES_API_COUNTRIES = ['us'];
+export const HTTP_BASIC_AUTH = 'http_basic_auth';
+export const JWT = 'jwt';
+export const ACCESS_TOKEN = 'access';
+export const REFRESH_TOKEN = 'refresh';
